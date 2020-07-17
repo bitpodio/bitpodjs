4239dff9c95d2c9653be5554553105553d5456fa
jagannath7775 <jagannath@bitpod.io>
2020-07-17 11:52:23 +0530

    feat: add grid search

--- components/common/grid/index.vue
+++ components/common/grid/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div>
+      <span @click="onFilterClick">filter</span>
+    </div>
     <div class="grid-search-section">
       <v-text-field
         v-model="search"
@@ -79,9 +82,21 @@ function getTableHeader(content, viewName) {
   return headers
 }
 
-function formatResult(data, modelName) {
+// function formatData(content, viewName, data) {
+//   const view = content.Views[viewName]
+//   let fields = view.Fields
+//   fields = Object.keys(fields)
+//   return data.map((record) => {
+//     return fields.map((field) => {
+
+//     })
+//   })
+// }
+
+function formatResult(content, viewName, data, modelName) {
   let { edges } = data[modelName][`${modelName}Find`]
   edges = edges.map(({ node }) => node)
+  // formatData(content, viewName, edges)
   return edges
 }
 
@@ -90,12 +105,33 @@ function formatCountData(data, modelName) {
   return count
 }
 
-function getOrderQuery(sortBy, sortDesc) {
+function getOrderQuery(content, viewName, sortBy, sortDesc) {
   // let {sortBy,sortDesc} = option;
+  if (!(sortBy && sortBy.length)) {
+    const view = content.Views[viewName]
+    const defaultSort = view.DefaultSort
+    return defaultSort || ''
+  }
   return `${sortBy && sortBy[0]} ${sortDesc && sortDesc[0] ? 'DESC' : 'ASC'}`
 }
 
-function buildSearchQueryVariables() {}
+function buildSearchQueryVariables(content, viewName, search) {
+  const view = content.Views[viewName]
+  const fields = view.Fields
+
+  let where = {}
+  const or = []
+  for (const field in fields) {
+    const { type, searchEnable } = fields[field]
+    if (type === 'string' && searchEnable) {
+      or.push({ [field]: { like: search, options: 'i' } })
+    }
+  }
+  where = {
+    or,
+  }
+  return where
+}
 
 export default {
   props: {
@@ -104,7 +140,11 @@ export default {
       required: true,
     },
     viewName: {
-      type: Object,
+      type: String,
+      required: true,
+    },
+    search: {
+      type: String,
       required: true,
     },
   },
@@ -121,7 +161,7 @@ export default {
       loading: 0,
       totalCount: 0,
       options: {},
-      search: '',
+      isFilterApplied: false,
     }
   },
   beforeDestroy() {
@@ -143,6 +183,9 @@ export default {
   // },
   methods: {
     updatePagination(pagination) {},
+    onFilterClick(e) {
+      this.isFilterApplied = true
+    },
   },
   apollo: {
     tableData: {
@@ -151,25 +194,28 @@ export default {
       `,
       variables() {
         // Use vue reactive properties here
-        // let {limit,skip} = this;
+        const { content, viewName, search } = this
         const sortBy = this.options.sortBy
         const sortDesc = this.options.sortDesc
         // let {,} = ;
-        const order = getOrderQuery(sortBy, sortDesc)
-        const where = buildSearchQueryVariables() || {}
+        const order = getOrderQuery(content, viewName, sortBy, sortDesc)
+        const where = search
+          ? buildSearchQueryVariables(content, viewName, search)
+          : {}
         const skip = (this.options.page - 1) * this.options.itemsPerPage
         const limit = this.options.itemsPerPage
         return {
           filters: { limit, skip, order, where },
-          where: {},
+          where,
         }
       },
       update(data) {
         console.log(data)
         // The returned value will update
         // the vue property 'pingMessage'
+        const { content, viewName } = this
         const tableData = {
-          items: formatResult(data, 'Event'),
+          items: formatResult(content, viewName, data, 'Event'),
           total: formatCountData(data, 'Event'),
         }
         return tableData
