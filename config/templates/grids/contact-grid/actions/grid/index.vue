3d6ea88edfecba73db184e3d07d19c13df7e8c7f
Amartya Roy <amartyakumar@bitpod.io>
2020-08-10 12:12:03 +0530

    feat: Contacts Added

---
+++ config/templates/grids/contact-grid/actions/grid/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>hehehehehhe</p>
+    <customItem1 />
+  </div>
+</template>
+
+<script>
+import customItem1 from './custom-item1'
+export default {
+  components: {
+    customItem1,
+  },
+}
+</script>
---
+++ config/templates/grids/contact-grid/actions/row-select/delete-item1.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-btn color="primary" dark @click="onDelete">
+      Delete
+    </v-btn>
+    <v-snackbar v-model="snackbar" timeout="2000" top="true">
+      Item deleted successfully.
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+function buildMutationDeleteQuery(modelName) {
+  return `mutation($Inputs: ${modelName}DeleteByIdInput!){ ${modelName} { ${modelName}DeleteById(input:$Inputs){ clientMutationId } } }`
+}
+
+// function getModelName(content, viewName) {
+//   const view = content.views[viewName]
+//   const dataSource = view.dataSource
+//   return dataSource.model
+// }
+
+export default {
+  //   mixins: [myMixin],
+  props: ['content', 'viewName', 'items'],
+  data() {
+    return {
+      dialog: false,
+      updateCount: 0,
+      snackbar: false,
+    }
+  },
+  beforeUpdate() {
+    console.log('update called')
+  },
+  methods: {
+    async onDelete() {
+      this.dialog = false
+      const modelName = 'Contacts'
+      const deleteItemMutation = buildMutationDeleteQuery(modelName)
+      const userDeleted = await this.$apollo.mutate({
+        mutation: gql(deleteItemMutation),
+        variables: {
+          Inputs: {
+            id: this.items[0].id,
+            clientMutationId: `${modelName} list item deleted successfully.`,
+          },
+        },
+      })
+      this.snackbar = true
+      console.log(userDeleted)
+    },
+  },
+}
+</script>
