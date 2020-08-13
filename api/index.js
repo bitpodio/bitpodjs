3e9533922077388d03626f75a9ca25c873b6966c
Amartya Roy <amartyakumar@bitpod.io>
2020-08-13 14:25:39 +0530

    Feat_grid_filter:Changed the variable names to camelCase in nuxtConfig files

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ const urlParser = require('url')
 const express = require('express')
 const app = express()
 
-const tokenEndpointUrl =
-  nuxtconfig.auth.strategies.bitpod.BITPOD_TOKEN_ENDPOINT_URL
-const UserinfoEndpointUrl =
-  nuxtconfig.auth.strategies.bitpod.BITPOD_USERINFO_ENDPOINT_URL
+const tokenEndPointUrl =
+  nuxtconfig.auth.strategies.bitpod.bitpodTokenEndPointUrl
+const userInfoEndPointUrl =
+  nuxtconfig.auth.strategies.bitpod.bitpodUserInfoEndPointUrl
 
 app.post('/connect/token', async (req, res) => {
   const params = new URLSearchParams()
@@ -22,7 +22,7 @@ app.post('/connect/token', async (req, res) => {
   params.append('code', code)
   params.append('redirect_uri', nuxtconfig.auth.strategies.bitpod.redirectUri)
 
-  const tokenResponse = await axios.post(tokenEndpointUrl, params)
+  const tokenResponse = await axios.post(tokenEndPointUrl, params)
 
   res.json({
     token_type: 'bearer',
@@ -37,7 +37,7 @@ app.get('/connect/userinfo', async (req, res) => {
       Authorization: req.headers.authorization,
     },
   }
-  const userResponse = await axios.get(UserinfoEndpointUrl, config)
+  const userResponse = await axios.get(userInfoEndPointUrl, config)
   res.json({
     Status: 200,
     data: userResponse.data,
