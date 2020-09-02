d1263a71144c61f8e8bacb1b46cf917b3746f8a9
Amartya Roy <amartyakumar@bitpod.io>
2020-09-02 10:32:06 +0530

    feat_edit_event_settings:added the form for edit event

--- pages/apps/_app/event/_id/editEventSetting.vue
+++ pages/apps/_app/event/_id/editEventSetting.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout>
+  <v-form ref="form" v-model="valid">
     <v-dialog
       v-model="eventSetting"
       persistent
@@ -8,28 +8,20 @@
       transition="dialog-bottom-transition"
     >
       <v-card>
-        <v-card-title class="px-15">
-          <span class="headline"></span>
+        <v-toolbar dense flat dark fixed color="accent">
+          <v-toolbar-title class="body-1">Edit Event Settings</v-toolbar-title>
           <v-spacer></v-spacer>
-          <div>
-            <v-btn text large color="primary" depressed @click.native="close"
-              ><v-icon left>mdi-check</v-icon>Save</v-btn
-            >
-            <v-btn class="mr-2" outlined color="primary"
-              ><v-icon left>mdi-check</v-icon>Save</v-btn
-            >
-          </div>
-          <div>
-            <v-btn icon depressed @click.native="close">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </div>
-        </v-card-title>
-        <v-card-text class="px-15 pt-4 px-xs-2">
+          <v-btn icon dark @click="close">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-divider></v-divider>
+        <v-card-text>
+          <!-- <v-card-text class="px-15 pt-4 px-xs-2"> -->
           <v-row>
-            <v-col cols="12">
+            <!-- <v-col cols="12">
               <h1 class="mb-3 black--text">Edit Event Settings</h1>
-            </v-col>
+            </v-col> -->
             <v-col cols="12" sm="6" md="6">
               <v-select
                 :items="['Public', 'Private']"
@@ -54,6 +46,7 @@
             </v-col>
             <v-col cols="12" sm="6" md="6">
               <v-text-field
+                v-model="formData.UniqLink"
                 label="Event Link*"
                 hint="https://bitpod-event.test.bitpod.io/e/"
                 persistent-hint
@@ -110,19 +103,70 @@
         </v-card-text>
       </v-card>
     </v-dialog>
-  </v-layout>
+  </v-form>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+import event from '~/config/apps/event/gql/event.gql'
+// import generalconfiguration from '~/config/apps/event/gql/registrationStatusOptions.gql'
+import { formatGQLResult } from '~/utility/gql.js'
 export default {
   props: {
     eventSetting: {
       default: false,
     },
   },
+  data() {
+    return {
+      valid: true,
+      tags: [],
+      tagsDropdown: [],
+      data: {
+        event: {},
+      },
+      formData: {},
+    }
+  },
   methods: {
     close() {
       this.$emit('update:eventSetting', false)
+    },
+  },
+  apollo: {
+    data: {
+      query() {
+        return gql`
+          ${event}
+        `
+      },
+      variables() {
+        return {
+          filters: {
+            where: {
+              id: this.$route.params.id,
+            },
+          },
+          eventId: this.$route.params.id,
+        }
+      },
+      update(data) {
+        debugger
+        const event = formatGQLResult(data, 'Event')
+        this.formData = event.length > 0 ? { ...event[0] } : {}
+        return {
+          event: event.length > 0 ? event[0] : {},
+        }
+      },
+      result({ data, loading, networkStatus }) {},
+      error(error) {
+        this.error = error
+        this.loading = 0
+      },
+      prefetch: false,
+      loadingKey: 'loading',
+      skip: false,
+      pollInterval: 0,
     },
   },
 }
