a85cf628f10a23c9809566a7a0bd9fa9a8d8ec20
rishikesh <rishikesh@bitpod.io>
2020-09-07 14:00:54 +0530

    logout template fixes added

--- layouts/logoutlayout.vue
+++ layouts/logoutlayout.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar app flat>
+    <v-app-bar app flat color="white">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <span class="bitpod-logo logo-ds">
         <v-img
@@ -9,9 +9,6 @@
         ></v-img>
       </span>
       <v-spacer></v-spacer>
-      <v-btn icon @click="$vuetify.theme.dark = !$vuetify.theme.dark">
-        <v-icon>mdi-invert-colors</v-icon>
-      </v-btn>
       <v-btn class="ma-2" outlined color="primary" to="/login">
         Sign in
       </v-btn>
