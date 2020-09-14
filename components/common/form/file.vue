e4f751b741d0c2117f47fc4eca06da651b93b4ca
Ankit Gupta <ankitkumar@bitpod.io>
2020-09-14 23:20:48 +0530

    fixed UI changed

--- components/common/form/file.vue
+++ components/common/form/file.vue
@@ -2,29 +2,38 @@
   <div style="position: relative;">
     <h3
       class="font-weight-regular"
-      style="position: absolute; top: 16px; left: 40px;"
-      @click="lableClicked"
+      style="position: absolute; top: -12px; left: 8px;"
     >
       {{ field.caption }}
     </h3>
+    <v-btn
+      class="blue white--text uploadFile"
+      small
+      :disabled="allowSecondUpload()"
+      @click="uploadClicked"
+      >Upload</v-btn
+    >
     <v-file-input
+      v-if="!allowSecondUpload()"
       ref="test"
+      class="pl-2"
       :multiple="field.multiple"
       :rules="rules"
       :hide-input="true"
       prepend-icon="mdi-cloud-upload"
       @change="onChange"
     ></v-file-input>
-    <div class="text-center">
-      <v-chip
+    <div :class="{ 'pt-12': allowSecondUpload() }">
+      <div
         v-for="file in files"
         :key="file.id"
-        class="ma-2"
-        close
-        @click:close="deleteFile(file.id)"
+        class="ma-2 grey lighten-2 pa-2 rounded-sm"
       >
-        {{ file.name }}
-      </v-chip>
+        <v-icon class="float-right" @click="deleteFile(file.id)"
+          >mdi-close</v-icon
+        >
+        <a :href="getDownloadLink(file.id)">{{ file.name }}</a>
+      </div>
     </div>
   </div>
 </template>
@@ -53,8 +62,14 @@ export default {
     }
   },
   methods: {
-    lableClicked() {
-      this.$refs.test.$el.firstElementChild.click()
+    allowSecondUpload() {
+      return !this.field.multiple && !!this.value && !!this.value.length
+    },
+    getDownloadLink(id) {
+      return `https://${nuxtconfig.axios.eventUrl}${nuxtconfig.axios.apiEndpoint}Attachments/download/${id}`
+    },
+    uploadClicked() {
+      this.$refs.test.$el.firstElementChild.firstElementChild.firstElementChild.click()
     },
     async onChange(files) {
       const fileUploadPromises = files.map((file) => this.uploadFile(file))
