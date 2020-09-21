059ff1bdad54c4533ded3b0ba13dc153833b300f
rishikesh <rishikesh@bitpod.io>
2020-09-21 09:26:05 +0530

    live draft event changes

---
+++ config/templates/grids/livedraft-grid/body.vue
@@ -0,0 +1,168 @@
+<template>
+  <v-flex class="greybg">
+    <div>
+      <div class="body-1 pb-2">Live Events</div>
+      <v-row class="ma-0">
+        <v-col
+          v-for="item in items"
+          :key="item.id"
+          class="pl-0 pt-0 eventtiles"
+          cols="12"
+          sm="4"
+          md="4"
+          lg="3"
+        >
+          <v-card class="elevation-1">
+            <!-- <div v-if="item.Images !== null" class="pa-1">
+              <v-card
+                v-for="image in item.Images"
+                :key="image"
+                class="d-inline-block elevation-0 tile-img"
+              >
+                <v-img
+                  :src="getAttachmentLink(image, true)"
+                  :lazy-src="getAttachmentLink(image, true)"
+                  aspect-ratio="1"
+                  class="grey lighten-2 tile-img"
+                >
+                  <template v-slot:placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5"
+                      ></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-card>
+            </div> -->
+            <div>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                lazy-src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                aspect-ratio="1"
+                class="tile-img"
+              >
+                <template v-slot:placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                  >
+                    <v-progress-circular
+                      indeterminate
+                      color="grey lighten-5"
+                    ></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+            </div>
+            <v-flex class="tile-info">
+              <div class="text--secondary pa-2 pb-0 body-2">
+                {{ item.StartDate }}
+              </div>
+              <v-card-title
+                class="text-h5 text-truncate d-block text-capitalize pa-2 pt-0 pb-1"
+              >
+                {{ item.Title }}
+              </v-card-title>
+              <div class="text--secondary pa-2 pt-0 body-2">
+                {{ item.VenueName }}
+              </div>
+            </v-flex>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-chip
+                v-for="Tags in item.Tags"
+                :key="Tags"
+                small
+                class="ma-0 mr-1"
+              >
+                {{ Tags }}
+              </v-chip>
+
+              <v-spacer></v-spacer>
+              <v-menu
+                left
+                bottom
+                :offset-y="offset"
+                transition="slide-y-transition"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn icon small v-bind="attrs" v-on="on">
+                    <v-icon>mdi-dots-vertical</v-icon>
+                  </v-btn>
+                </template>
+
+                <v-list dense>
+                  <v-list-item>
+                    <v-list-item-icon class="mr-2">
+                      <i class="fa fa-clone mt-1" aria-hidden="true"></i>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>Make a copy</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-icon class="mr-2">
+                      <i class="fa fa-eye mt-1" aria-hidden="true"></i>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>Preview</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-icon class="mr-2">
+                      <i
+                        class="fa fa-pencil-square-o mt-1"
+                        aria-hidden="true"
+                      ></i>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>Edit</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+  </v-flex>
+</template>
+
+<script>
+import nuxtconfig from '~/nuxt.config'
+export default {
+  props: ['items'],
+  methods: {
+    formatAddressField(fieldValue) {
+      return fieldValue || ' '
+    },
+    getAttachmentLink(id, isDownloadLink) {
+      debugger
+      const attachmentUrl = `https://${nuxtconfig.axios.eventUrl}${
+        nuxtconfig.axios.apiEndpoint
+      }Attachments${isDownloadLink ? '/download' : ''}${id ? '/' + id : ''}`
+      return attachmentUrl
+    },
+  },
+}
+</script>
+
+<style scoped>
+.tile-img {
+  max-height: 170px;
+}
+.eventtiles {
+  max-width: 300px;
+}
+.tile-info {
+  min-height: 90px;
+}
+</style>
