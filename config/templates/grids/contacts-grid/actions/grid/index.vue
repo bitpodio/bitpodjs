869744e8e54de00e1a6273e1d972ad9b70beb724
Ankit Gupta <ankitkumar@bitpod.io>
2020-09-22 12:17:52 +0530

    added button

---
+++ config/templates/grids/contacts-grid/actions/grid/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-flex">
+    <newItem
+      :content="content"
+      :view-name="viewName"
+      :on-new-item-save="onNewItemSave"
+      :refresh="refresh"
+      :context="context"
+    />
+    <div class="mt-3">
+      <campaing />
+    </div>
+  </div>
+</template>
+
+<script>
+import campaing from '~/config/templates/grids/eventInvites-grid/actions/grid/new-item.vue'
+import newItem from '~/config/common/templates/grid/actions/grid/new-item.vue'
+export default {
+  components: {
+    campaing,
+    newItem,
+  },
+  props: ['content', 'viewName', 'onNewItemSave', 'refresh', 'context'],
+}
+</script>
