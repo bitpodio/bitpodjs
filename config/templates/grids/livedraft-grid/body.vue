69447a5b0fed944cb77a55522dd0c0b541b45e7b
rishikesh <rishikesh@bitpod.io>
2020-09-23 15:48:45 +0530

    event tiles UI fixes

--- config/templates/grids/livedraft-grid/body.vue
+++ config/templates/grids/livedraft-grid/body.vue
@@ -13,11 +13,11 @@
           lg="3"
         >
           <div v-if="item.BusinessType === 'Recurring'">
-            <nuxt-link
-              :to="recurringRoutes(item.id)"
-              class="text-decoration-none"
-            >
-              <v-card class="elevation-1 pa-2">
+            <v-card class="elevation-1 pa-2">
+              <nuxt-link
+                :to="recurringRoutes(item.id)"
+                class="text-decoration-none"
+              >
                 <div v-if="!item.Images.length" class="overflow-h rounded-0">
                   <v-flex
                     class="tile-img tile-pattern rounded-0"
@@ -54,7 +54,7 @@
                     {{ item.StartDate }}
                   </div>
                   <v-card-title
-                    class="text-h5 text-truncate d-block text-capitalize pa-2 pt-0 pb-1 pl-0"
+                    class="text-h5 grey--text text--darken-4 text-truncate d-block text-capitalize pa-2 pt-0 pb-1 pl-0"
                   >
                     {{ item.Title }}
                   </v-card-title>
@@ -65,68 +65,68 @@
                   </div>
                 </v-flex>
                 <v-divider></v-divider>
-                <v-card-actions class="px-0">
-                  <div class="text-truncate d-block">
-                    <v-chip
-                      v-for="Tags in item.Tags"
-                      :key="Tags"
-                      small
-                      class="ma-0 mr-1"
-                    >
-                      {{ Tags }}
-                    </v-chip>
-                  </div>
-
-                  <v-spacer></v-spacer>
-                  <v-menu
-                    left
-                    bottom
-                    :offset-y="offset"
-                    transition="slide-y-transition"
+              </nuxt-link>
+              <v-card-actions class="px-0">
+                <div class="text-truncate d-block">
+                  <v-chip
+                    v-for="Tags in item.Tags"
+                    :key="Tags"
+                    small
+                    class="ma-0 mr-1"
                   >
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn icon small v-bind="attrs" v-on="on">
-                        <v-icon>mdi-dots-vertical</v-icon>
-                      </v-btn>
-                    </template>
+                    {{ Tags }}
+                  </v-chip>
+                </div>
 
-                    <v-list dense>
-                      <v-list-item @click="viewRegistration(item.UniqLink)">
-                        <v-list-item-icon class="mr-2">
-                          <i class="fa fa-eye mt-1" aria-hidden="true"></i>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                          <v-list-item-title>Preview</v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                      <v-list-item>
-                        <v-list-item-icon class="mr-2">
-                          <i
-                            class="fa fa-pencil-square-o mt-1"
-                            aria-hidden="true"
-                          ></i>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                          <v-list-item-title>Edit</v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                      <v-list-item>
-                        <v-list-item-icon class="mr-2">
-                          <i class="fa fa-clone mt-1" aria-hidden="true"></i>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                          <v-list-item-title>Make a copy</v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                    </v-list>
-                  </v-menu>
-                </v-card-actions>
-              </v-card>
-            </nuxt-link>
+                <v-spacer></v-spacer>
+                <v-menu
+                  left
+                  bottom
+                  :offset-y="offset"
+                  transition="slide-y-transition"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon small v-bind="attrs" v-on="on">
+                      <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                  </template>
+
+                  <v-list dense>
+                    <v-list-item @click="viewRegistration(item.UniqLink)">
+                      <v-list-item-icon class="mr-2">
+                        <i class="fa fa-eye mt-1" aria-hidden="true"></i>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>Preview</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-icon class="mr-2">
+                        <i
+                          class="fa fa-pencil-square-o mt-1"
+                          aria-hidden="true"
+                        ></i>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>Edit</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-icon class="mr-2">
+                        <i class="fa fa-clone mt-1" aria-hidden="true"></i>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>Make a copy</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-card-actions>
+            </v-card>
           </div>
           <div v-else>
-            <nuxt-link :to="routes(item.id)" class="text-decoration-none">
-              <v-card class="elevation-1 pa-2">
+            <v-card class="elevation-1 pa-2">
+              <nuxt-link :to="routes(item.id)" class="text-decoration-none">
                 <div v-if="!item.Images.length" class="overflow-h rounded-0">
                   <v-flex
                     class="tile-img tile-pattern rounded-0"
@@ -163,7 +163,7 @@
                     {{ item.StartDate }}
                   </div>
                   <v-card-title
-                    class="text-h5 text-truncate d-block text-capitalize pa-2 pt-0 pb-1 pl-0"
+                    class="text-h5 grey--text text--darken-4 text-truncate d-block text-capitalize pa-2 pt-0 pb-1 pl-0"
                   >
                     {{ item.Title }}
                   </v-card-title>
@@ -174,64 +174,64 @@
                   </div>
                 </v-flex>
                 <v-divider></v-divider>
-                <v-card-actions class="px-0">
-                  <div class="text-truncate d-block">
-                    <v-chip
-                      v-for="Tags in item.Tags"
-                      :key="Tags"
-                      small
-                      class="ma-0 mr-1"
-                    >
-                      {{ Tags }}
-                    </v-chip>
-                  </div>
-
-                  <v-spacer></v-spacer>
-                  <v-menu
-                    left
-                    bottom
-                    :offset-y="offset"
-                    transition="slide-y-transition"
+              </nuxt-link>
+              <v-card-actions class="px-0">
+                <div class="text-truncate d-block">
+                  <v-chip
+                    v-for="Tags in item.Tags"
+                    :key="Tags"
+                    small
+                    class="ma-0 mr-1"
                   >
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn icon small v-bind="attrs" v-on="on">
-                        <v-icon>mdi-dots-vertical</v-icon>
-                      </v-btn>
-                    </template>
+                    {{ Tags }}
+                  </v-chip>
+                </div>
 
-                    <v-list dense>
-                      <v-list-item @click="viewRegistration(item.UniqLink)">
-                        <v-list-item-icon class="mr-2">
-                          <i class="fa fa-eye mt-1" aria-hidden="true"></i>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                          <v-list-item-title>Preview</v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                      <v-list-item>
-                        <v-list-item-icon class="mr-2">
-                          <i
-                            class="fa fa-pencil-square-o mt-1"
-                            aria-hidden="true"
-                          ></i>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                          <v-list-item-title>Edit</v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                      <v-list-item>
-                        <v-list-item-icon class="mr-2">
-                          <i class="fa fa-clone mt-1" aria-hidden="true"></i>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                          <v-list-item-title>Make a copy</v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                    </v-list>
-                  </v-menu>
-                </v-card-actions>
-              </v-card>
-            </nuxt-link>
+                <v-spacer></v-spacer>
+                <v-menu
+                  left
+                  bottom
+                  :offset-y="offset"
+                  transition="slide-y-transition"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon small v-bind="attrs" v-on="on">
+                      <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                  </template>
+
+                  <v-list dense>
+                    <v-list-item @click="viewRegistration(item.UniqLink)">
+                      <v-list-item-icon class="mr-2">
+                        <i class="fa fa-eye mt-1" aria-hidden="true"></i>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>Preview</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-icon class="mr-2">
+                        <i
+                          class="fa fa-pencil-square-o mt-1"
+                          aria-hidden="true"
+                        ></i>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>Edit</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-icon class="mr-2">
+                        <i class="fa fa-clone mt-1" aria-hidden="true"></i>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>Make a copy</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-card-actions>
+            </v-card>
           </div>
         </v-col>
       </v-row>
