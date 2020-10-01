0899eb546f983225aee7cfe487f5e5bb81e28b39
jagannath7775 <jagannath@bitpod.io>
2020-10-02 02:32:00 +0530

    :sprinkles: typo fix

--- utility/oauth.js
+++ utility/oauth.js
@@ -1,8 +1,10 @@
 import authenticationPopups from 'authentication-popups'
 
+const POPUP_DIMENSIONS = { width: 400, height: 640 }
+
 export const openAuthPopups = (options, callback) => {
   const path = `/oauth/index.html?${queryParams(options)}`
-  authenticationPopups(path, { width: 400, height: 640 })
+  authenticationPopups(path, POPUP_DIMENSIONS)
   window.authAgent.removeAllListeners('suspend')
   window.authAgent.once('suspend', function (event) {
     if (typeof callback === 'function') {
