5da8aac5fac63897374aab3685cccf4ed5da3c63
Amartya Roy <amartyakumar@bitpod.io>
2020-10-06 23:38:13 +0530

    feat_org_user_actions:Added the access key and adding user functionality in the org section

---
+++ config/templates/grids/accessKey-grid/actions/grid/new-item.vue
@@ -0,0 +1,139 @@
+<template>
+  <v-col class="px-0">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      scrollable
+      content-class="slide-form-default"
+      transition="dialog-bottom-transition"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn text small v-bind="attrs" v-on="on" @click="dialog = true">
+          <v-icon left>mdi-plus</v-icon> New
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title
+          class="pl-md-10 pl-lg-10 pl-xl-15 pr-1 pb-0 pt-1 d-flex align-start font-weight-regular"
+        >
+          <h2 class="black--text font-weight-regular pt-10 pb-9">
+            Create Access Key
+          </h2>
+          <v-spacer></v-spacer>
+          <div>
+            <v-btn icon @click="onClose">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </div>
+        </v-card-title>
+        <v-card-text class="px-xs-2 px-md-10 px-lg-10 px-xl-15 pt-0">
+          <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+            <v-row>
+              <v-col cols="12" sm="10" md="8" class="pb-0">
+                <v-datetime-picker
+                  v-model="accessKeyDate"
+                  label="Expires On"
+                  :text-field-props="startDateTextFieldProps"
+                >
+                  <template slot="dateIcon">
+                    <v-icon>fas fa-calendar</v-icon>
+                  </template>
+                  <template slot="timeIcon">
+                    <v-icon>fas fa-clock</v-icon>
+                  </template>
+                </v-datetime-picker>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions
+          class="px-xs-3 px-md-10 px-lg-10 px-xl-15 px-xs-10 pl-xs-10"
+        >
+          <v-btn color="primary" depressed @click="onSave">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-col>
+</template>
+
+<script>
+import { getApiUrl } from '~/utility'
+export default {
+  props: {
+    refresh: {
+      type: Function,
+      default: () => false,
+    },
+    item: {
+      type: Object,
+      default: () => {},
+      required: false,
+    },
+  },
+  data() {
+    return {
+      accessKeyDate: '',
+      formData: {},
+      dialog: false,
+      value: '',
+    }
+  },
+  computed: {
+    startDateTextFieldProps() {
+      return {
+        appendIcon: 'fa-calendar',
+        outlined: true,
+        dense: true,
+        rules: [
+          (v) => {
+            const scheduledDate = v && new Date(v)
+            if (scheduledDate && scheduledDate > this.formData.EndDate) {
+              this.valid = false
+              return 'Ticket start date should be less than Ticket end date'
+            } else {
+              this.valid = true
+              return true
+            }
+          },
+        ],
+      }
+    },
+  },
+  methods: {
+    onReset() {
+      this.$refs.form.reset()
+    },
+    onClose() {
+      this.$refs.form.reset()
+      this.dialog = false
+    },
+    async onSave() {
+      const url = getApiUrl()
+      this.formData.expiresat = this.accessKeyDate
+      const userId = this.$auth.$state.user.data.email
+      try {
+        const res = await this.$axios.$post(
+          `${url}Users/${userId}/Accesskey`,
+          this.formData
+        )
+        if (res) {
+          this.dialog = false
+          this.onReset()
+          this.refresh()
+        }
+      } catch (e) {
+        console.log(
+          `Error in templates/grids/accessKey-grid/actions/grid/new-item.vue while making a POST call to Users model from method onSave context:-URL:-${url}\nformData:-${this.formData} `,
+          e
+        )
+      }
+    },
+  },
+}
+</script>
+<style scoped>
+.v-picker {
+  border-radius: 0;
+}
+</style>
---
+++ config/templates/grids/accessKey-grid/column-3.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-flex>
+    <div>
+      <v-snackbar v-model="snackbar" :timeout="2000" :top="true" width="10">
+        <div class="toast text-center">
+          {{ snackbarText }}
+        </div>
+      </v-snackbar>
+      <v-menu left bottom :offset-y="offset" transition="slide-y-transition">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon small v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list dense>
+          <v-list-item class="cursorPointer">
+            <v-list-item-content @click="openSnackbar">
+              <copy
+                :text-to-copy="getKeyCopied(item.accesskey, item.accesssecret)"
+                label="Copy"
+                :hide-toast="true"
+                :unique-id="'a' + item.id"
+                icon-size="15"
+              />
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item class="cursorPointer">
+            <v-list-item-icon class="mr-2">
+              <i class="fa fa-trash mt-1" aria-hidden="true"></i>
+            </v-list-item-icon>
+            <v-list-item-content
+              class="ml-n3"
+              @click="onDelete(item.accesskey, item.userId)"
+            >
+              <v-list-item-title>Delete</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+  </v-flex>
+</template>
+
+<script>
+import copy from '~/components/common/copy'
+import { getApiUrl } from '~/utility'
+export default {
+  components: {
+    copy,
+  },
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+      required: false,
+    },
+    refresh: {
+      type: Function,
+      required: false,
+      default: () => false,
+    },
+  },
+  data() {
+    return {
+      snackbar: false,
+      snackbarText: '',
+    }
+  },
+  methods: {
+    openSnackbar() {
+      this.snackbar = true
+      this.snackbarText = 'Access Key and Secret copied to clipboard'
+    },
+    getKeyCopied(accessKey, secretKey) {
+      const key = `key:${accessKey}secret:${secretKey}`
+      return key
+    },
+    async onDelete(accessKey, userName) {
+      const url = getApiUrl()
+      const check = confirm('Are you sure you want to delete')
+      if (check === true) {
+        try {
+          const res = await this.$axios.$delete(
+            `${url}Users/${userName}/Accesskey/${accessKey}`
+          )
+          this.snackbar = true
+          this.snackbarText = 'Item deleted successfully'
+          this.refresh()
+          return res
+        } catch (e) {
+          console.log(
+            `Error in templates/grids/accessKey-grid/actions/grid/new-item.vue while making a DELETE call to Users model from method onDelete context:-URL:-${url}\n userName:${userName}\naccessKey:${accessKey}`,
+            e
+          )
+        }
+      }
+    },
+  },
+}
+</script>
