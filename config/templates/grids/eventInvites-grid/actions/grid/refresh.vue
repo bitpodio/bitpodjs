fc4a81a0d976a33ea9d4c5b308a1d5bb981533bf
Lokesh Popat <lpopat@paragyte.com>
2020-10-08 14:52:54 +0530

    Added RTE dropdowns and refresh button on grid

---
+++ config/templates/grids/eventInvites-grid/actions/grid/refresh.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-col class="px-0">
+    <v-btn text small v-bind="attrs" v-on="on" @click="refreshInvites">
+      <v-icon left>fa-refresh-ccw</v-icon> Refresh
+    </v-btn>
+  </v-col>
+</template>
+<script>
+export default {
+  props: {
+    refresh: {
+      type: Function,
+      required: false,
+      default: () => false,
+    },
+  },
+  methods: {
+    refreshInvites() {
+      this.$parent.$parent.refresh()
+    },
+  },
+}
+</script>
