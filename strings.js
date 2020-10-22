0cdcf5edfd089e9add04e6cbde66255362e75df4
KapilChothe1 <kapil@bitpod.io>
2020-10-22 17:25:48 +0530

    added edit and delete actions functionality of admin badge template

--- strings.js
+++ strings.js
@@ -34,4 +34,6 @@ module.exports = Object.freeze({
   REFUND_PAID_AMT:
     'Refunded Amount can not be greater or equal than total paid amount',
   REFUND_AMT_MSG: 'Amount should not be less than or equal to  0',
+
+  INVALID_DISPLAY_ORDER: 'Display order should not be blank and less than 1',
 })
