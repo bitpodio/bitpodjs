7a8790e42abb103a96203fd9e1e406cc22ef9d0e
jagannath7775 <jagannath@bitpod.io>
2020-10-25 23:11:10 +0530

    remove logs

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -25,21 +25,18 @@ const isValidPage = (store, route) => {
 
 export default function (context) {
   const { store, route, redirect, $auth } = context
-  // If the user is not authorized
-  console.log('in auth ', process.server)
 
   if (process.server) {
     const { req } = context
     const hostName = req.headers.host
     const publicDomain = process.env.PUBLIC_DOMAIN
-    console.log('hostName', Object.keys(req))
-    console.log('PUBLIC_PATH', process.env.PUBLIC_DOMAIN)
     if (hostName === publicDomain) {
       const provider = $auth.strategy.name
-      console.log('provider', provider)
       const currentOrgName = store.state.currentOrg.name
       const basePath = process.env.PUBLIC_PATH || ''
-      return redirect(`http://${currentOrgName}-${publicDomain}${basePath}`)
+      return redirect(
+        `http://${currentOrgName}-${publicDomain}${basePath}/login?p=${provider}`
+      )
     }
   }
   if (!isValidPage(store, route)) {
