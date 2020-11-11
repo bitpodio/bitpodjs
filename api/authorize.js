f91aede146de9ee6b08aabcb28120b82d4d14624
Amartya Roy <amartyakumar@bitpod.io>
2020-11-11 14:08:59 +0530

    bug_16:added consoleLogs

--- api/authorize.js
+++ api/authorize.js
@@ -2,21 +2,31 @@ import qs from 'querystring'
 import url from 'url'
 import nuxtconfig from '../nuxt.config'
 export default function (req, res, next) {
+  console.log('===>in authorize.js')
   const referer = req.headers.referer
+  console.log('===>in authorize.js Referer', referer)
   if (!referer) {
+    console.log('===>in authorize.js inside referer', referer)
     return next()
   }
+  console.log('===>in authorize.js outside referer', referer)
   // eslint-disable-next-line node/no-deprecated-api
   const host = url.parse(referer).host
+  console.log('===>in authorize.js host==>', host)
   // parsing the url to get the query parameters and get the info from which provider the request is being made
   const query = qs.parse(req.url.substring(2).replace('?', '&'))
+  console.log('===>in authorize.js query==>', query)
   const oldState = query.state
+  console.log('===>in authorize.js oldState==>', oldState)
   query.state = Buffer.from(JSON.stringify({ host, oldState })).toString(
     'base64'
   )
+  console.log('===>in authorize.js  query.state==>', query.state)
   const authorizationUrl =
     nuxtconfig.auth.strategies[query.provider].authorization
+  console.log('===>in authorize.js  authorizationUrl==>', authorizationUrl)
   delete query.provider
+  console.log('===>in authorize.js  query1==>', query)
   res.writeHead(301, {
     Location: `${authorizationUrl}?${qs.encode(query)}`,
   })
--- api/callback.js
+++ api/callback.js
@@ -1,14 +1,34 @@
 import qs from 'querystring'
 import nuxtconfig from '../nuxt.config'
 export default function (req, res, next) {
+  console.log('===>in callback.js  req==>', req)
+  console.log('===>in callback.js  res==>', res)
   const query = qs.parse(req.url.substring(2))
+  console.log('===>in callback.js  query==>', query)
   if (!query.cbVerified) {
+    console.log(
+      '===>in callback.js  inside query.cbVerified==>',
+      query.cbVerified
+    )
     const { state } = query
+    console.log('===>in callback.js  inside query.cbVerified state==>', state)
     let stateObject
     if (state) {
+      console.log(
+        '===>in callback.js inside query.cbVerified  inside state==>',
+        state
+      )
       const decodedState = Buffer.from(state, 'base64').toString('ascii')
+      console.log(
+        '===>in callback.js inside query.cbVerified  inside decodedState==>',
+        decodedState
+      )
       try {
         stateObject = JSON.parse(decodedState)
+        console.log(
+          '===>in callback.js inside query.cbVerified  inside stateObject==>',
+          stateObject
+        )
       } catch (err) {
         console.error('state is not a valid json')
         console.error('State : ', decodedState)
@@ -16,6 +36,16 @@ export default function (req, res, next) {
         return next()
       }
       const { host, oldState } = stateObject
+      console.log(
+        '===>in callback.js inside query.cbVerified  inside stateObject==>',
+        stateObject
+      )
+      console.log(
+        `location:http://${host}${nuxtconfig.router.base}callback?${qs.encode(
+          query
+        )}`
+      )
+
       query.cbVerified = true
       query.state = oldState
       res.writeHead(301, {
