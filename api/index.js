2da02e3822c8725fca34339e7187385b43782b9d
Amartya Roy <amartyakumar@bitpod.io>
2020-11-18 21:45:02 +0530

    bug_16:login url changes on click back button

--- api/index.js
+++ api/index.js
@@ -7,37 +7,16 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 function getTokenUserInfoUrl(ob) {
-  console.log('===>in api/index.js inside getTokenUserInfoUrl Ob:', ob)
   const provider = ob.provider
-  console.log(
-    '===>in api/index.js inside getTokenUserInfoUrl provider:',
-    provider
-  )
   let tokenUrl, userInfoUrl
   switch (provider) {
     case 'google':
       tokenUrl = nuxtconfig.auth.strategies.google.tokenEndPointUrl
       userInfoUrl = nuxtconfig.auth.strategies.google.userInfoEndPointUrl
-      console.log(
-        '===>in api/index.js inside getTokenUserInfoUrl tokenUrl:',
-        tokenUrl
-      )
-      console.log(
-        '===>in api/index.js inside getTokenUserInfoUrl userInfoUrl:',
-        userInfoUrl
-      )
       break
     case 'bitpod':
       tokenUrl = nuxtconfig.auth.strategies.bitpod.tokenEndPointUrl
       userInfoUrl = nuxtconfig.auth.strategies.bitpod.userInfoEndPointUrl
-      console.log(
-        '===>in api/index.js inside getTokenUserInfoUrl tokenUrl:',
-        tokenUrl
-      )
-      console.log(
-        '===>in api/index.js inside getTokenUserInfoUrl userInfoUrl:',
-        userInfoUrl
-      )
       break
     default:
       console.log(`No provider added in the key: ${provider}`)
@@ -46,17 +25,12 @@ function getTokenUserInfoUrl(ob) {
     tokenEndPointUrl: tokenUrl,
     userInfoEndPointUrl: userInfoUrl,
   }
-  console.log('===>in api/index.js inside getTokenUserInfoUrl urls:', urls)
   return urls
 }
 
 app.post('/connect/token', async (req, res) => {
-  console.log('===>in api/index.js inside /connect/token req:', req)
-  console.log('===>in api/index.js inside /connect/token res:', res)
   const params = new URLSearchParams()
-  console.log('===>in api/index.js inside /connect/token params:', params)
   const urls = getTokenUserInfoUrl(req.query)
-  console.log('===>in api/index.js inside /connect/token urls:', urls)
   params.append(
     'client_id',
     nuxtconfig.auth.strategies[req.query.provider].clientId
@@ -65,44 +39,19 @@ app.post('/connect/token', async (req, res) => {
     'client_secret',
     nuxtconfig.auth.strategies[req.query.provider].clientSecret
   )
-  console.log('===>in api/index.js inside /connect/token params:', params)
   if (req.body.refresh_token) {
     const refreshToken = req.body.refresh_token
     params.append('grant_type', 'refresh_token')
     params.append('refresh_token', refreshToken)
-    console.log(
-      '===>in api/index.js inside /connect/token If refresh_token:',
-      params
-    )
     try {
       const tokenResponse = await axios.post(urls.tokenEndPointUrl, params)
-      console.log(
-        '===>in api/index.js inside /connect/token tokenResponse:',
-        tokenResponse
-      )
       if (req.query.provider === 'google') {
-        console.log(
-          '===>in api/index.js inside /connect/token id_token:',
-          tokenResponse.data.id_token
-        )
-        console.log(
-          '===>in api/index.js inside /connect/token refresh_token:',
-          tokenResponse.data.refresh_token
-        )
         res.json({
           token_type: 'bearer',
           access_token: tokenResponse.data.id_token,
           refresh_token: tokenResponse.data.refresh_token,
         })
       } else {
-        console.log(
-          '===>in api/index.js inside /connect/token access_token:',
-          tokenResponse.data.access_token
-        )
-        console.log(
-          '===>in api/index.js inside /connect/token refresh_token:',
-          tokenResponse.data.refresh_token
-        )
         res.json({
           token_type: 'bearer',
           access_token: tokenResponse.data.access_token,
@@ -118,46 +67,21 @@ app.post('/connect/token', async (req, res) => {
     }
   } else {
     const code = req.body.code
-    console.log('===>in api/index.js inside /connect/token else code:', code)
     params.append('grant_type', 'authorization_code')
     params.append('code', code)
     params.append(
       'redirect_uri',
       nuxtconfig.auth.strategies[req.query.provider].redirectUri
     )
-    console.log(
-      '===>in api/index.js inside /connect/token else params:',
-      params
-    )
     try {
       const tokenResponse = await axios.post(urls.tokenEndPointUrl, params)
-      console.log(
-        '===>in api/index.js inside /connect/token else tokenResponse:',
-        tokenResponse
-      )
       if (req.query.provider === 'google') {
-        console.log(
-          '===>in api/index.js inside /connect/token else id_token:',
-          tokenResponse.data.id_token
-        )
-        console.log(
-          '===>in api/index.js inside /connect/token else refresh_token:',
-          tokenResponse.data.refresh_token
-        )
         res.json({
           token_type: 'bearer',
           access_token: tokenResponse.data.id_token,
           refresh_token: tokenResponse.data.refresh_token,
         })
       } else {
-        console.log(
-          '===>in api/index.js inside /connect/token else access_token:',
-          tokenResponse.data.access_token
-        )
-        console.log(
-          '===>in api/index.js inside /connect/token else refresh_token:',
-          tokenResponse.data.refresh_token
-        )
         res.json({
           token_type: 'bearer',
           access_token: tokenResponse.data.access_token,
@@ -173,19 +97,12 @@ app.post('/connect/token', async (req, res) => {
 
 app.get('/connect/userinfo', async (req, res) => {
   let userData
-  console.log('===>in api/index.js inside /connect/userinfo  in req:', req)
-  console.log('===>in api/index.js inside /connect/userinfo  in res:', res)
   const urls = getTokenUserInfoUrl(req.query)
-  console.log('===>in api/index.js inside /connect/userinfo  in urls:', urls)
   const config = {
     headers: {
       Authorization: req.headers.authorization,
     },
   }
-  console.log(
-    '===>in api/index.js inside /connect/userinfo  in config:',
-    config
-  )
   try {
     if (req.query.provider === 'google') {
       const userResponse = await axios.get(
@@ -194,23 +111,11 @@ app.get('/connect/userinfo', async (req, res) => {
         }?id_token=${req.headers.authorization.replace('Bearer ', '')}`
       )
       userData = userResponse.data
-      console.log(
-        '===>in api/index.js inside /connect/userinfo  in userResponse:',
-        userData
-      )
     } else {
       const userResponse = await axios.get(urls.userInfoEndPointUrl, config)
       userData = userResponse.data
-      console.log(
-        '===>in api/index.js inside /connect/userinfo else in userResponse:',
-        userData
-      )
     }
     const userOrgsDetails = await fetchUserOrgsDetails(req)
-    console.log(
-      '===>in api/index.js inside /connect/userinfo else in userOrgsDetails:',
-      userOrgsDetails
-    )
     const userDetails = {
       ...userOrgsDetails,
       ...userData,
@@ -225,10 +130,6 @@ app.get('/connect/userinfo', async (req, res) => {
 })
 
 function getAxiosConfig(req) {
-  console.log(
-    '===>in api/index.js inside /connect/userinfo else in getAxiosConfig req.headers.authorization:',
-    req.headers.authorization
-  )
   return {
     headers: {
       Authorization: req.headers.authorization,
@@ -239,8 +140,6 @@ function getAxiosConfig(req) {
 function getApiUrl(req) {
   const host = req.get('host')
   const protocol = req.protocol
-  console.log('===>in api/index.js inside  getApiUrl host:', host)
-  console.log('===>in api/index.js inside getApiUrl protocol:', protocol)
   if (host && host.includes('localhost')) {
     return `${protocol}://${nuxtconfig.axios.backendBaseUrl}`
   }
@@ -248,16 +147,9 @@ function getApiUrl(req) {
 }
 
 async function fetchUserOrgs(req) {
-  console.log('===>in api/index.js inside fetchUserOrgs req:', req)
   const origin = getApiUrl(req)
   const config = getAxiosConfig(req)
   const apiEndpoint = nuxtconfig.axios.apiEndpoint
-  console.log('===>in api/index.js inside fetchUserOrgs origin:', origin)
-  console.log('===>in api/index.js inside fetchUserOrgs config:', config)
-  console.log(
-    '===>in api/index.js inside fetchUserOrgs apiEndpoint:',
-    apiEndpoint
-  )
   let userOrgList = {}
   try {
     const orgResponse = await axios.get(
@@ -265,10 +157,6 @@ async function fetchUserOrgs(req) {
       config
     )
     userOrgList = orgResponse.data
-    console.log(
-      '===>in api/index.js inside fetchUserOrgs userOrgList:',
-      userOrgList
-    )
   } catch (error) {
     userOrgList = {
       error,
