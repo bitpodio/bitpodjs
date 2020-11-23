c4b660bbdc9556274568e5810d740537395a7ce7
rishikesh <rishikesh@bitpod.io>
2020-11-23 16:26:39 +0530

    added roles grid bottom space

---
+++ config/templates/grids/roles-grid/column-0.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="key-align">
+    {{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+      required: false,
+    },
+    value: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    context: {
+      type: Object,
+      default: () => {},
+      required: false,
+    },
+  },
+}
+</script>
