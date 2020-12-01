581e12fd0875d4b0f90e4ab3b0bdb229d37a8ce8
Ankit Gupta <ankitkumar@bitpod.io>
2020-12-02 02:13:00 +0530

    added custom roomname support

--- pages/apps/_app/live/_id/index.vue
+++ pages/apps/_app/live/_id/index.vue
@@ -87,6 +87,9 @@ export default {
     },
     onIFrameLoad() {
       const jitsi = this.$refs.jitsiRef
+      if (this.$route.query.n) {
+        jitsi.executeCommand('subject', this.$route.query.n)
+      }
       jitsi.addEventListener('participantRoleChanged', (e) => {
         if (e.role === 'moderator') {
           jitsi.executeCommand('password', this.$route.params.id.split('-')[2])
