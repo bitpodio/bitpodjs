ef8dd3b70d6cc4adb51f1961b4d5c29e66654ef9
rishikesh <rishikesh@bitpod.io>
2020-12-02 17:09:28 +0530

    added chat iframe in lounge page

--- pages/apps/_app/attendee/_id/index.vue
+++ pages/apps/_app/attendee/_id/index.vue
@@ -962,6 +962,28 @@
               </v-skeleton-loader>
             </v-flex>
           </div>
+          <div
+            class="xs12 sm4 md4 lg4 boxview boxviewsmall pa-3 mb-4 mx-0 ml-md-2 mr-0 pb-2 rounded-lg"
+          >
+            <v-flex class="d-flex justify-center align-center pb-2">
+              <h2 class="body-1 pb-0">
+                <i class="fa-message-square pr-1" aria-hidden="true"></i>
+                <i18n path="Common.Chat" />
+              </h2>
+              <v-spacer></v-spacer>
+            </v-flex>
+            <v-divider></v-divider>
+            <div class="body-1 mt-2">
+              <iframe
+                id="rcChannel"
+                name="rcChannel"
+                :src="`https://chat.bitpod.io/channel/${registration.EventId}?layout=embedded`"
+                width="100%"
+                height="300"
+                frameBorder="0"
+              ></iframe>
+            </div>
+          </div>
         </v-flex>
       </v-flex>
     </v-flex>
@@ -1006,6 +1028,7 @@ export default {
   },
   mounted() {
     this.getRegistrationData()
+    this.authenticateIFrame()
   },
   methods: {
     formatDate(date) {
@@ -1152,6 +1175,15 @@ export default {
           )
         }
       }
+    },
+    authenticateIFrame() {
+      document.getElementById('rcChannel').contentWindow.postMessage(
+        {
+          externalCommand: 'login-with-token',
+          token: 'eFC45gJz8vrh7Tngn1SRwETemv2kDKgDAMjNlEqH9s2',
+        },
+        '*'
+      )
     },
   },
 }
