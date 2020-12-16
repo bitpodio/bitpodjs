e8753c123d8c0b0955e8932abaf74b3a4214d955
Yash Dave <yash@bitpod.io>
2020-12-16 20:15:14 +0530

    re-position checkbox

--- config/templates/grids/eventAttendees-grid/item.vue
+++ config/templates/grids/eventAttendees-grid/item.vue
@@ -1,19 +1,18 @@
 <template>
   <tr>
     <td>
-      <div class="d-flex justify-end pt-4 pb-2">
-        <v-simple-checkbox :value="isSelected" @input="select($event)">
+      <div class="d-flex align-center py-4 mx-n3">
+        <v-simple-checkbox
+          class="pr-3"
+          :value="isSelected"
+          @input="select($event)"
+        >
         </v-simple-checkbox>
-      </div>
-      <div class="d-flex align-center pb-4">
-        <v-avatar tile color="primary" size="56" class="mx-2">
-          <span class="white--text Twitter">{{ item.FullName }}</span>
-        </v-avatar>
         <div class="flex-grow-1">
           <p class="mb-1 fs-larger text-capitalize">{{ item.FullName }}</p>
           <ColumnStatus :value="item.Status" :outlined="true" :label="true" />
         </div>
-        <div class="">
+        <div class="min-w-checkin">
           <ColumnCheckin
             :item="item"
             :context="context"
@@ -25,7 +24,6 @@
     </td>
   </tr>
 </template>
-
 <script>
 import ColumnCheckin from './column-checkin.vue'
 import ColumnStatus from './column-status.vue'
@@ -61,11 +59,9 @@ export default {
   },
 }
 </script>
-
 <style scoped>
-.align-checkbox {
-  right: 18px;
-  padding-top: 10px;
+.min-w-checkin {
+  min-width: 88px;
 }
 .fs-larger {
   font-size: 1.25rem;
