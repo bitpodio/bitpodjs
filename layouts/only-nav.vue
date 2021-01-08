47c8a6aa5c5d5f34f082fb00854970d0fd7b12a2
Yash Dave <yash@bitpod.io>
2021-01-08 15:07:07 +0530

    clean commit 2 for get-started

---
+++ layouts/only-nav.vue
@@ -0,0 +1,134 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app flat class="greybg headernew pl-0" height="50">
+      <v-toolbar-title
+        class="ml-n3 pl-0 px-2 py-1 logo-ds d-flex align-center appbar-left"
+      >
+        <v-app-bar-nav-icon
+          :ripple="false"
+          size="24"
+          height="36px"
+          width="36px"
+          class="ml-0 ml-md-2 mr-2 mr-md-3"
+          @click.stop="drawer = !drawer"
+        ></v-app-bar-nav-icon>
+        <span class="bitpod-logo logo-ds d-none d-sm-flex">
+          <v-img
+            :src="$config.cdnUri + 'bitpod-logo-new.png'"
+            class="logofull mr-2"
+          ></v-img>
+        </span>
+        <v-spacer></v-spacer>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div v-if="$auth.$state.loggedIn">
+        <v-menu
+          v-model="account"
+          :close-on-content-click="false"
+          :nudge-width="180"
+          offset-y
+          transition="slide-y-transition"
+          bottom
+        >
+          <template
+            v-if="$auth.$state.loggedIn"
+            v-slot:activator="{ on, attrs }"
+          >
+            <v-avatar color="primary ml-2" size="30" v-bind="attrs" v-on="on">
+              <span class="white--text">{{
+                $auth.user.data.name && $auth.user.data.name[0]
+              }}</span>
+            </v-avatar>
+          </template>
+          <v-card>
+            <v-list>
+              <v-list-item>
+                <v-list-item-avatar size="48">
+                  <v-avatar color="primary" size="48" v-bind="attrs" v-on="on">
+                    <span class="white--text headline">{{
+                      $auth.user.data.name && $auth.user.data.name[0]
+                    }}</span>
+                  </v-avatar>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{
+                    $auth.user.data.name
+                  }}</v-list-item-title>
+                  <v-list-item-subtitle>{{
+                    $auth.user.data.email
+                  }}</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-list dense class="pt-0">
+              <v-list-item>
+                <v-btn text small color="primary" @click="onLogout">
+                  <i18n path="Common.Logout" />
+                </v-btn>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-menu>
+      </div>
+      <div v-else>
+        <v-btn class="ma-2" outlined color="primary" to="/login">
+          <i18n path="Common.Login" />
+        </v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-main class="greybg seatmap">
+      <v-container fluid>
+        <v-row>
+          <v-col class="pt-0">
+            <div>
+              <nuxt />
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <Help />
+  </v-app>
+</template>
+
+<script>
+import Help from '~/components/common/help'
+export default {
+  layout: 'only-nav',
+  middleware: ['auth', 'authorization'],
+  components: {
+    Help,
+  },
+  data() {
+    return {
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
+      modal: false,
+      menu2: false,
+      drawer: null,
+      dialog1: false,
+      dialog: false,
+      notifications: false,
+      sound: true,
+      tabs: null,
+      account: false,
+      message: false,
+    }
+  },
+  async created() {
+    let token = this.$auth.strategy.token.get()
+    if (token) {
+      token = token.split(' ')[1]
+    }
+    await this.$apolloHelpers.onLogin(token, undefined, { expires: 7 })
+  },
+  methods: {
+    async onLogout() {
+      this.$auth.logout()
+      await this.$apolloHelpers.onLogout()
+    },
+  },
+}
+</script>
