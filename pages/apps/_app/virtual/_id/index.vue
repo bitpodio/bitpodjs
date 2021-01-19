bbd82f7e3109d6d484f6ab5e79eef788a6634f37
rishikesh <rishikesh@bitpod.io>
2021-01-19 13:39:52 +0530

    added autoplay class

--- pages/apps/_app/virtual/_id/index.vue
+++ pages/apps/_app/virtual/_id/index.vue
@@ -171,6 +171,7 @@
                       class="video-js vjs-default-skin"
                       controls
                       preload="auto"
+                      autoplay="true"
                       width="100%"
                       height="400"
                       data-setup="{}"
