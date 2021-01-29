c101acac345c2044903078edf82683568308b419
Amartya Roy <amartyakumar@bitpod.io>
2021-01-29 08:54:38 +0530

    added loggers in userApps

--- utility/userApps.js
+++ utility/userApps.js
@@ -5,12 +5,16 @@ const userCurrentOrgInfo = (store) => {
   const orgList = store.state.auth.user
     ? store.state.auth.user.data.orgList
     : []
+  console.log('currentOrg in userApps.js', currentOrg)
+  console.log('orgList in userApps.js', orgList)
   const currentDetails =
     Array.isArray(orgList) &&
     orgList.filter((org) => {
       return org.name === currentOrg.name
     })
+  console.log('currentDetails in userApps.js', currentDetails)
   const userOrgInfo = _get(currentDetails, '0')
+  console.log('userOrgInfo in userApps.js', userOrgInfo)
   return userOrgInfo || {}
 }
 
