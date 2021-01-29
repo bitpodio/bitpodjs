e219d2dea41c022a7aaa2e596e62851b4ef22ff7
Amartya Roy <amartyakumar@bitpod.io>
2021-01-29 20:37:21 +0530

    linting error

--- store/index.js
+++ store/index.js
@@ -81,7 +81,7 @@ export const mutations = {
     console.log('payload in numutation in setCurrentOrgInfo', payload)
     state.currentOrgInfo = payload
   },
-  setCommitError(state,payload){
+  setCommitError(state, payload) {
     state.commitError = payload
   },
   setExportInProgress(state, status) {
