fdbfd069918014781bbf026667c00786b231d613
Amartya Roy <amartyakumar@bitpod.io>
2021-01-30 21:28:45 +0530

    updated the asyn call of organization api

--- store/index.js
+++ store/index.js
@@ -6,13 +6,16 @@ export const actions = {
     const origin = 'https://bitpod-event.test.bitpod.io/svc/api/'
     console.log('coookiesAll', context.app.$cookies.getAll())
     try {
-      const orgInfoPromise = await context.$axios.get(
-        `${origin}OrganizationInfos`
-      )
-      console.log('received orgInfoPromise', orgInfoPromise)
-      // const [currentInfoRes] = await Promise.all([orgInfoPromise])
-      console.log('received currentOrgRes', orgInfoPromise.data[0])
-      commit('setCurrentOrgInfo', orgInfoPromise && orgInfoPromise.data[0])
+      // const orgInfoPromise = await context.$axios.get(
+      //   `${origin}OrganizationInfos`
+      // )
+      // console.log('received orgInfoPromise', orgInfoPromise)
+      // // const [currentInfoRes] = await Promise.all([orgInfoPromise])
+      // console.log('received currentOrgRes', orgInfoPromise.data[0])
+      // commit('setCurrentOrgInfo', orgInfoPromise && orgInfoPromise.data[0])
+      const orgPromise = await context.$axios.get(`${origin}Organizations/this`)
+      console.log('received orgPromise', orgPromise)
+      commit('setCurrentOrg', orgPromise.data)
     } catch (err) {
       console.log('error in catch', err)
     }
@@ -21,6 +24,7 @@ export const actions = {
 
 export const state = () => ({
   currentOrgInfo: {},
+  currentOrg: [],
 })
 
 export const mutations = {
@@ -28,5 +32,10 @@ export const mutations = {
     console.log('payload in numutation in setCurrentOrgInfo', payload)
     state.currentOrgInfo = payload
     console.log('sucess in setting in setCurrentOrgInfo')
+  },
+  setCurrentOrg(state, payload) {
+    console.log('payload in numutation in setCurrentOrg', payload)
+    state.currentOrg = payload
+    console.log('sucess in setting in setCurrentOrg')
   },
 }
