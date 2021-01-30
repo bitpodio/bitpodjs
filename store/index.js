b230cc84a7e5f58889f06df3a6b937b72fcdcebe
Amartya Roy <amartyakumar@bitpod.io>
2021-01-30 20:36:37 +0530

    made changes in api call

--- store/index.js
+++ store/index.js
@@ -1,14 +1,30 @@
 export const actions = {
   async nuxtServerInit({ commit }, context) {
-    console.log('context in nuxtServerInit', JSON.stringify(context))
+    console.log('context in nuxtServerInit', context)
+    console.log('headers of axios call', context.$axios.defaults.headers)
+    console.log('baseURL: of axios call', context.$axios.defaults.baseURL)
     const origin = 'https://bitpod-event.test.bitpod.io/svc/api/'
     console.log('coookiesAll', context.app.$cookies.getAll())
     try {
       const orgInfoPromise = context.$axios.get(`${origin}OrganizationInfos`)
-      const [currentInfoRes] = await Promise.all(orgInfoPromise)
+      console.log('received orgInfoPromise', orgInfoPromise)
+      const [currentInfoRes] = await Promise.all([orgInfoPromise])
       console.log('received currentOrgRes', currentInfoRes.data)
+      commit('setCurrentOrgInfo', currentInfoRes && currentInfoRes.data[0])
     } catch (err) {
       console.log('error in catch', err)
     }
+  },
+}
+
+export const state = () => ({
+  currentOrgInfo: {},
+})
+
+export const mutations = {
+  setCurrentOrgInfo(state, payload) {
+    console.log('payload in numutation in setCurrentOrgInfo', payload)
+    state.currentOrgInfo = payload
+    console.log('sucess in setting in setCurrentOrgInfo')
   },
 }
