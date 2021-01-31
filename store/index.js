57ea6ca85c0c83f59caf4c92d48048fdd6d5d13e
Amartya Roy <amartyakumar@bitpod.io>
2021-01-31 18:35:30 +0530

    added a dispatch method in nuxtServer

--- store/index.js
+++ store/index.js
@@ -1,36 +1,57 @@
-import nuxtconfig from '../nuxt.config'
+// import nuxtconfig from '../nuxt.config'
 export const actions = {
-  async nuxtServerInit({ commit }, context) {
-    const origin = getApiUrl(
-      context.req.headers.host,
-      context.req,
-      context.$config
-    )
-    const apiEndpoint = nuxtconfig.axios.apiEndpoint
-    try {
-      const orgPromise = context.$axios.get(
-        `${origin}${apiEndpoint}Organizations/this`
-      )
-      const orgInfoPromise = context.$axios.get(
-        `${origin}${apiEndpoint}OrganizationInfos`
-      )
-      const [currentOrgRes, currentOrgInfoRes] = await Promise.all([
-        orgPromise,
-        orgInfoPromise,
-      ])
-      const currentOrgInfo = currentOrgInfoRes.data
-      commit('setCurrentOrg', currentOrgRes.data)
-      commit('setCurrentOrgInfo', currentOrgInfo && currentOrgInfo[0])
-      if (context && context.req.headers.host.includes('localhost')) {
-        const postLogoutUrl = `http://${context.req.headers.host}/`
-        context.app.$cookies.set('auth.domain_url', postLogoutUrl)
-      } else {
-        const postLogoutUrl = `https://${context.req.headers.host}${this.$router.options.base}`
-        context.app.$cookies.set('auth.domain_url', postLogoutUrl)
-      }
-    } catch (err) {
-      commit('setCurrentOrg', err)
-    }
+  // async nuxtServerInit({ commit }, context) {
+  //   const origin = getApiUrl(
+  //     context.req.headers.host,
+  //     context.req,
+  //     context.$config
+  //   )
+  //   const apiEndpoint = nuxtconfig.axios.apiEndpoint
+  //   try {
+  //     const orgPromise = context.$axios.get(
+  //       `${origin}${apiEndpoint}Organizations/this`
+  //     )
+  //     const orgInfoPromise = context.$axios.get(
+  //       `${origin}${apiEndpoint}OrganizationInfos`
+  //     )
+  //     const [currentOrgRes, currentOrgInfoRes] = await Promise.all([
+  //       orgPromise,
+  //       orgInfoPromise,
+  //     ])
+  //     const currentOrgInfo = currentOrgInfoRes.data
+  //     commit('setCurrentOrg', currentOrgRes.data)
+  //     commit('setCurrentOrgInfo', currentOrgInfo && currentOrgInfo[0])
+  //     if (context && context.req.headers.host.includes('localhost')) {
+  //       const postLogoutUrl = `http://${context.req.headers.host}/`
+  //       context.app.$cookies.set('auth.domain_url', postLogoutUrl)
+  //     } else {
+  //       const postLogoutUrl = `https://${context.req.headers.host}${this.$router.options.base}`
+  //       context.app.$cookies.set('auth.domain_url', postLogoutUrl)
+  //     }
+  //   } catch (err) {
+  //     commit('setCurrentOrg', err)
+  //   }
+  // },
+  async nuxtServerInit({ commit, dispatch }, context) {
+    console.log('dispatching from the serverInit')
+    await dispatch('storeOrganizationInfo')
+    console.log('dispatched from the storeOrganizationInfo')
+    console.log('dispatching for the storeCurrentOrg')
+    await dispatch('storeCurrentOrg')
+    console.log('dispatched from the storeCurrentOrg')
+  },
+
+  async storeOrganizationInfo({ commit }, context) {
+    const origin = 'https://bitpod-event.test.bitpod.io/svc/api/'
+    const res = await this.$axios.get(`${origin}OrganizationInfos`)
+    console.log('response from Orginfo', res.data[0])
+    commit('setCurrentOrgInfo', res.data[0])
+  },
+  async storeCurrentOrg({ commit }, context) {
+    const origin = 'https://bitpod-event.test.bitpod.io/svc/api/'
+    const res = await this.$axios.get(`${origin}Organizations/this`)
+    console.log('response from currentOrg', res.data)
+    commit('setCurrentOrg', res.data)
   },
 }
 
@@ -43,12 +64,12 @@ export const state = () => ({
 
 export const mutations = {
   setCurrentOrg(state, payload) {
-    console.log('payload in setCurrentOrg',payload)
+    console.log('payload in setCurrentOrg', payload)
     state.currentOrg = payload
     console.log('success payload in setCurrentOrg')
   },
   setCurrentOrgInfo(state, payload) {
-    console.log('payload in setCurrentOrgInfo',payload)
+    console.log('payload in setCurrentOrgInfo', payload)
     state.currentOrgInfo = payload
     console.log('success payload in setCurrentOrg')
   },
@@ -60,10 +81,10 @@ export const mutations = {
   },
 }
 
-function getApiUrl(host, req, config) {
-  if (host && host.includes('localhost')) {
-    return `https://${config.axios.backendBaseUrl}`
-  }
-  // const protocol = req.protocol
-  return `https://${host}`
-}
+// function getApiUrl(host, req, config) {
+//   if (host && host.includes('localhost')) {
+//     return `https://${config.axios.backendBaseUrl}`
+//   }
+//   // const protocol = req.protocol
+//   return `https://${host}`
+// }
--- utility/userApps.js
+++ utility/userApps.js
@@ -1,14 +1,14 @@
 import { _get } from '~/utility/object.js'
 
 const userCurrentOrgInfo = (store) => {
-  const currentOrg = store.state.currentOrg.name || 'bitpod'
+  const currentOrg = store.state.currentOrg
   const orgList = store.state.auth.user
     ? store.state.auth.user.data.orgList
     : []
   const currentDetails =
     Array.isArray(orgList) &&
     orgList.filter((org) => {
-      return org.name === currentOrg
+      return org.name === currentOrg.name
     })
   const userOrgInfo = _get(currentDetails, '0')
   return userOrgInfo || {}
