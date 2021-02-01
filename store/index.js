eb48a90cf0dc819be96f9d5920af0a5dd617b15f
Amartya Roy <amartyakumar@bitpod.io>
2021-02-01 11:03:06 +0530

    added token and commented code

--- store/index.js
+++ store/index.js
@@ -59,19 +59,19 @@ export const actions = {
     // )
     // const parsedCookie = cookieparser.parse(req.headers.cookie)
     // console.log('ParsedCookie', parsedCookie)
-    if (context && context.req.headers.host.includes('localhost')) {
-      const apiUrl = `http://${context.req.headers.host}/`
-      console.log('apiUrl', apiUrl)
-    } else {
-      const apiUrl = `https://${context.req.headers.host}${this.$router.options.base}`
-      console.log('apiUrl', apiUrl)
-    }
+    // if (context && context.req.headers.host.includes('localhost')) {
+    //   const apiUrl = `http://${context.req.headers.host}/`
+    //   console.log('apiUrl', apiUrl)
+    // } else {
+    //   const apiUrl = `https://${context.req.headers.host}${this.$router.options.base}`
+    //   console.log('apiUrl', apiUrl)
+    // }
     const token =
-      'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjAzYjJkMjJjMmZlY2Y4NzNlZDE5ZTViOGNmNzA0YWZiN2UyZWQ0YmUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI0OTkyODM5MDk1MC1wbXU0bDczZnU2bXBjaW0yZ2RuZXJxZjYyazZvcHBxdC5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjQ5OTI4MzkwOTUwLXBtdTRsNzNmdTZtcGNpbTJnZG5lcnFmNjJrNm9wcHF0LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTAzNzUxMjU1NTM0NDczOTE0NTE4IiwiaGQiOiJiaXRwb2QuaW8iLCJlbWFpbCI6ImFtYXJ0eWFrdW1hckBiaXRwb2QuaW8iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IkE1akNFLUNkeXBYY2ZuQ2pDTWlEX3ciLCJuYW1lIjoiQW1hcnR5YWt1bWFyIFJveSIsInBpY3R1cmUiOiJodHRwczovL2xoNS5nb29nbGV1c2VyY29udGVudC5jb20vLThhRUJZQkNZNXQwL0FBQUFBQUFBQUFJL0FBQUFBQUFBQUFBL0FNWnV1Y2w0cnF4eXNSYXpiTWZkT2t0cEhETHQxYV91NkEvczk2LWMvcGhvdG8uanBnIiwiZ2l2ZW5fbmFtZSI6IkFtYXJ0eWFrdW1hciIsImZhbWlseV9uYW1lIjoiUm95IiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2MTIxNTMyNzQsImV4cCI6MTYxMjE1Njg3NH0.ozC3VA6fzW2lUbDkEhOsTOk8Nh4CM7mKWVE6jvWc18GpcZWs-ZgDsxlcgklSfbhHAEGJA7knMXlK_9IwN-Z5hS67bd7OAJ9EGZXdxaX7W63nbjgMpiOzZIEHYRJCoth2sB2WVsWqRRXlFBJDCS486P-YXw_vcIiL_lZrH0eW62cAk9D6N9DKGUVICsNxHWrlJZDmeJi8ObKJMUO6F64zD6L4SLDyeQBb8jcN1TX7EIQjIWRD3Fpus9COD4qfU-D56SN7Dqoo9Ev4bFTjfNIbFB4mJIl2mrQr2wZ2NaKKnOY7Pbm5sllW64vCmK00OE6IuDa9hxemSTjabRHvZFsdjQ'
+      'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjAzYjJkMjJjMmZlY2Y4NzNlZDE5ZTViOGNmNzA0YWZiN2UyZWQ0YmUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI0OTkyODM5MDk1MC1wbXU0bDczZnU2bXBjaW0yZ2RuZXJxZjYyazZvcHBxdC5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjQ5OTI4MzkwOTUwLXBtdTRsNzNmdTZtcGNpbTJnZG5lcnFmNjJrNm9wcHF0LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTAzNzUxMjU1NTM0NDczOTE0NTE4IiwiaGQiOiJiaXRwb2QuaW8iLCJlbWFpbCI6ImFtYXJ0eWFrdW1hckBiaXRwb2QuaW8iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IkFPaGdjRnZSNHQwMWtQVTBFUDRPMWciLCJuYW1lIjoiQW1hcnR5YWt1bWFyIFJveSIsInBpY3R1cmUiOiJodHRwczovL2xoNS5nb29nbGV1c2VyY29udGVudC5jb20vLThhRUJZQkNZNXQwL0FBQUFBQUFBQUFJL0FBQUFBQUFBQUFBL0FNWnV1Y2w0cnF4eXNSYXpiTWZkT2t0cEhETHQxYV91NkEvczk2LWMvcGhvdG8uanBnIiwiZ2l2ZW5fbmFtZSI6IkFtYXJ0eWFrdW1hciIsImZhbWlseV9uYW1lIjoiUm95IiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2MTIxNTY4NDcsImV4cCI6MTYxMjE2MDQ0N30.FoP7pblUWus3tgMvrZA3osFZsFvwCFiv0B1IED2iTToAJ0vISzkwBshgXeY4O9AVvYeCR1SlseRTyKhWHB2vvrBzl5te9RuyJ4d79MJI6QICX64CB-hsiriCifiJKEtneH6sfyzcs60hdSI07Dc_5uq1h5JF4lk4YENbs8e4kdRntXiSf0WfRNiuVuOwAWAHjhM79MnUVguEBxeX0gWcQ0s8_EnMkCkg4ckGoRp9rkmiGrIHmYjFSu6e4EDinvhOCilEUtj3TEaT1iDIlNevKu8nDUVezophuiSNB1P2_AvNNghXL1jVmoyICbZYKx28rjh5ZGgTv9D7U4R-C1WWGA'
     const headers = {
       Authorization: token,
     }
-    const origin = 'http://bitpod-event.test.bitpod.io/svc/api/'
+    const origin = 'https://bitpod-event.test.bitpod.io/svc/api/'
     try {
       const res = await this.$axios.get(`${origin}Organizations/this`, {
         headers,
