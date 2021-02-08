7a03ddae224a123fc12cb126838bf70b5d40d434
Yash Dave <yash@bitpod.io>
2021-02-08 11:42:49 +0530

    adjust error timings and output on get started

--- pages/get-started/index.vue
+++ pages/get-started/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-snackbar v-model="snackbar" :timeout="2000" :top="true" width="2px">
+    <v-snackbar v-model="snackbar" :timeout="3000" :top="true" width="2px">
       <div class="fs-16 text-center">
         {{ snackbarText }}
       </div>
@@ -326,7 +326,7 @@ export default {
                   if (jobInfo._SetupErrors.length) {
                     this.snackbarText = this.$t('Messages.Error.SetupOrgFailed')
                     this.snackbar = true
-                    this.tab = 1
+                    this.tab = 3
                     this.processing = false
                     this.resetBtn = !this.resetBtn
                   } else if (jobInfo._SetupStatus.length) {
@@ -373,7 +373,7 @@ export default {
               } catch (err) {
                 this.snackbarText = this.$t('Messages.Error.SetupOrgFailed')
                 this.snackbar = true
-                this.tab = 1
+                this.tab = 3
                 this.processing = false
                 this.resetBtn = !this.resetBtn
                 console.error(
@@ -386,14 +386,14 @@ export default {
           } else {
             this.snackbarText = this.$t('Messages.Error.SetupOrgFailed')
             this.snackbar = true
-            this.tab = 1
+            this.tab = 3
             this.processing = false
             this.resetBtn = !this.resetBtn
           }
         } catch (err) {
           this.snackbarText = this.$t('Messages.Error.SetupOrgFailed')
           this.snackbar = true
-          this.tab = 1
+          this.tab = 3
           this.processing = false
           this.resetBtn = !this.resetBtn
           console.error(
