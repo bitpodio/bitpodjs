5fee58d5c2877d7e83da9a20c8f7a713877c3a3e
Yash Dave <yash@bitpod.io>
2021-02-24 12:07:42 +0530

    remove base path from nuxt redirect

--- middleware/helpRedirect.js
+++ middleware/helpRedirect.js
@@ -1,8 +1,5 @@
 export default function (ctx) {
   if (ctx.route.fullPath === '/apps/help-center') {
-    ctx.redirect(
-      301,
-      `${ctx.$config.basePublicPath}/apps/help-center/introduction/quick-start`
-    )
+    ctx.redirect(301, `/apps/help-center/introduction/quick-start`)
   }
 }
