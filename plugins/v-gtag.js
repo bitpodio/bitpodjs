051fe178eb817a05ececed105d995b06e8b482af
rishikesh <rishikesh@bitpod.io>
2021-03-12 17:13:29 +0530

    trackId changed

--- plugins/v-gtag.js
+++ plugins/v-gtag.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 
 Vue.use(VueAnalytics, {
-  id: 'AW-824538838',
-  // id: 'UA-191875244-1',
+  // id: 'AW-824538838',
+  id: 'UA-191875244-1',
 })
