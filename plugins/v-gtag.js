d43d3628a2cca4b568cd741573361d7f69cf7f35
rishikesh <rishikesh@bitpod.io>
2021-03-15 14:38:04 +0530

    gtag added

--- plugins/v-gtag.js
+++ plugins/v-gtag.js
@@ -1,17 +1,7 @@
 import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 
 Vue.use(VueAnalytics, {
   // id: 'AW-824538838',
   id: 'UA-191875244-1',
-  router,
 })
-
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app')
