be49fd6ce5b5c954e276d6cb6f98be280b6b3c70
Amartya Roy <amartyakumar@bitpod.io>
2021-03-18 16:57:12 +0530

    fixed the api call using debounced methd

--- components/newSingleEvent.vue
+++ components/newSingleEvent.vue
@@ -51,6 +51,7 @@
                     v-model="eventData.Title"
                     :rules="[rules.required]"
                     :label="$t('Common.EventTitle')"
+                    debounce="500"
                     required
                     dense
                     outlined
@@ -115,11 +116,12 @@
                     v-model="eventData.UniqLink"
                     :label="$t('Common.EventL')"
                     :rules="[rules.required, rules.link]"
+                    debounce="500"
                     outlined
                     dense
                     required
                     :error-messages="uniqueLinkValidationMsg"
-                    @input="checkUniqueLink"
+                    @input="updateEventUniqLink"
                   ></v-text-field>
                 </v-col>
               </v-row>
@@ -547,11 +549,12 @@ import addMonths from 'date-fns/addMonths'
 import addDays from 'date-fns/addDays'
 import { zonedTimeToUtc } from 'date-fns-tz'
 import gql from 'graphql-tag'
+import _ from 'lodash'
 import CustomDate from '~/components/common/form/date.vue'
 import Lookup from '~/components/common/form/lookup.vue'
 import registrationStatusOptions from '~/config/apps/event/gql/registrationStatusOptions.gql'
 import Timezone from '~/components/common/form/timezone'
-import eventCount from '~/config/apps/event/gql/eventCount.gql'
+// import eventCount from '~/config/apps/event/gql/eventCount.gql'
 import orgInfoLocationType from '~/config/apps/event/gql/orgInfoLocationType.gql'
 import { formatGQLResult } from '~/utility/gql.js'
 import nuxtconfig from '~/nuxt.config'
@@ -695,6 +698,7 @@ export default {
           TicketCount: 100,
         },
       ],
+      eventCount: 0,
     }
   },
   computed: {
@@ -798,26 +802,83 @@ export default {
   },
 
   methods: {
+    updateEventTitle: _.debounce(function (event) {
+      this.checkUniqueLink(event)
+    }, 500),
+    updateEventUniqLink: _.debounce(function (event) {
+      this.checkUniqueLink()
+    }, 500),
     async checkUniqueLink() {
-      const where = { UniqLink: this.eventData.UniqLink }
-      const result = await this.$apollo.query({
-        query: gql`
-          ${eventCount}
-        `,
-        variables: {
-          where,
-        },
-        fetchPolicy: 'no-cache',
-      })
-      if (result.data.Event.EventCount > 0) {
-        this.isInvalidEventLink = true
-        this.isUniqLinkValid = false
-        this.uniqueLinkMessage = this.$t('Messages.Error.UniqueLinkDuplicate')
-      } else {
-        this.isInvalidEventLink = false
-        this.isUniqLinkValid = true
-        this.uniqueLinkMessage = ''
+      console.log('checking the UniquLink===>')
+      const filter = { where: { UniqLink: this.eventData.UniqLink } }
+      // const where = { UniqLink: this.eventData.UniqLink }
+      // const result = await this.$apollo.query({
+      //   query: gql`
+      //     ${eventCount}
+      //   `,
+      //   variables: {
+      //     where,
+      //   },
+      //   fetchPolicy: 'no-cache',
+      // })
+      // const eventFilter = {'where':{
+      //   UniqueLink:
+      // }}
+      try {
+        const res = await this.$axios.get(
+          `${this.$bitpod.getApiUrl()}Events?filter=${JSON.stringify(filter)}`
+        )
+        console.log('res==>', res)
+        if (res.data.length > 0) {
+          this.eventCount = res.data.length
+          console.log('got the result data and its gt then 0', res.data.length)
+          this.isInvalidEventLink = true
+          console.log('this.isInvalidEventLink', this.isInvalidEventLink)
+          this.isUniqLinkValid = false
+          console.log('this.isUniqLinkValid', this.isUniqLinkValid)
+          this.uniqueLinkMessage = this.$t('Messages.Error.UniqueLinkDuplicate')
+        } else {
+          debugger
+          this.eventCount = res.data.length
+          console.log('event link ', this.eventData.UniqLink)
+          console.log('got the result data and its lt then 0', res.data.length)
+          console.log('no error')
+          this.isInvalidEventLink = false
+          console.log('this.isInvalidEventLink', this.isInvalidEventLink)
+          this.isUniqLinkValid = true
+          console.log('this.isUniqLinkValid', this.isUniqLinkValid)
+          this.uniqueLinkMessage = ''
+        }
+      } catch (e) {
+        console.log('error', e)
       }
+
+      // if (result.data.Event.EventCount > 0) {
+      //   this.eventCount = result.data.Event.EventCount
+      //   console.log(
+      //     'got the result data and its gt then 0',
+      //     result.data.Event.EventCount
+      //   )
+      //   this.isInvalidEventLink = true
+      //   console.log('this.isInvalidEventLink', this.isInvalidEventLink)
+      //   this.isUniqLinkValid = false
+      //   console.log('this.isUniqLinkValid', this.isUniqLinkValid)
+      //   this.uniqueLinkMessage = this.$t('Messages.Error.UniqueLinkDuplicate')
+      // } else {
+      //   debugger
+      //   this.eventCount = result.data.Event.EventCount
+      //   console.log('event link ', this.eventData.UniqLink)
+      //   console.log(
+      //     'got the result data and its lt then 0',
+      //     result.data.Event.EventCount
+      //   )
+      //   console.log('no error')
+      //   this.isInvalidEventLink = false
+      //   console.log('this.isInvalidEventLink', this.isInvalidEventLink)
+      //   this.isUniqLinkValid = true
+      //   console.log('this.isUniqLinkValid', this.isUniqLinkValid)
+      //   this.uniqueLinkMessage = ''
+      // }
     },
     ticketCountRules() {
       return [
@@ -1348,7 +1409,7 @@ export default {
       if (regex.test(value)) {
         if (isNaN(value)) {
           this.eventData.UniqLink = value
-          this.checkUniqueLink(this.eventData.UniqLink)
+          this.updateEventTitle(this.eventData.UniqLink)
         }
       } else {
         this.isUniqLinkValid = false
