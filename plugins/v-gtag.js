ddd55f3e4c9e3efadad7648b40cc651229220db5
rishikesh <rishikesh@bitpod.io>
2021-03-18 13:22:18 +0530

    gtm and tarcking code added

--- plugins/v-gtag.js
+++ plugins/v-gtag.js
@@ -1,14 +1,8 @@
 import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 
-// Vue.use(VueAnalytics, {
-//   // id: 'UA-191875244-1',
-//   id: 'UA-192083281-1',
-// })
-
 export default function ({ $config }) {
   Vue.use(VueAnalytics, {
-    // id: 'UA-192083281-1',
-    id: $config.gaGtmCode,
+    id: $config.gaTrackingCode,
   })
 }
--- plugins/v-gtm.js
+++ plugins/v-gtm.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import VueTagManager from 'vue-tag-manager'
 
-// Do this before creating the Vue app
 export default function ({ $config }) {
   Vue.use(VueTagManager, {
-    // gtmId: 'GTM-54VGK22',
-    gtmId: $config.gaTrackingCode,
+    gtmId: $config.gaGtmCode,
   })
 }
