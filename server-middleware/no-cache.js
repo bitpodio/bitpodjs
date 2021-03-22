35335ac8526fc50c86698163ed4cb580497ee0c1
Yash Dave <yash@bitpod.io>
2021-03-22 11:07:20 +0530

    add no-cache to help-center pages

--- server-middleware/no-cache.js
+++ server-middleware/no-cache.js
@@ -1,5 +1,8 @@
 export default function (req, res, next) {
-  if (req.url === '/apps/event/eventboard') {
+  if (
+    req.url === '/apps/event/eventboard' ||
+    req.url.includes('/apps/help-center/')
+  ) {
     res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
     res.setHeader('Pragma', 'no-cache')
     res.setHeader('Expires', '0')
