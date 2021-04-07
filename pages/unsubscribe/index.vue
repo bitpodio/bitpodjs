eb528584eab26c89197e9309e3c2553973667990
Amartya Roy <amartyakumar@bitpod.io>
2021-04-07 12:03:46 +0530

    Fixed locale issue

--- pages/unsubscribe/index.vue
+++ pages/unsubscribe/index.vue
@@ -11,13 +11,13 @@
           <v-card-title
             class="pl-md-10 pl-lg-10 pl-xl-15 pr-1 pt-4 pb-4 d-flex align-start"
           >
-            <h2 class="black--text pt-4 pb-0 text-h5">
+            <h2 class="black--text pb-0 text-h5">
               <i18n path="Drawer.UnSubscribeAction" />
             </h2>
           </v-card-title>
           <v-divider class="ml-10 mr-11"></v-divider>
           <v-card-text class="px-xs-2 px-md-10 px-lg-10 px-xl-15 pt-4">
-          <h2 class="black--text pt-4 pb-0 text-h5">
+            <h2 class="black--text pb-0 text-h5">
               <i18n path="Drawer.UnSubscribeMsg" />
             </h2>
             <v-row class="unsubscribeBox pt-4 mt-3 ml-1 mr-1">
@@ -62,8 +62,8 @@
           <v-card-title
             class="pl-md-10 pl-lg-10 pl-xl-15 pr-1 pt-4 pb-4 d-flex align-start"
           >
-            <h2>
-              You have unsubscribed successfully
+            <h2 class="black--text pt-4 pb-0 text-h5">
+              <i18n path="Drawer.UnsubscribeSuccessMsg" />
             </h2>
           </v-card-title>
         </v-card>
