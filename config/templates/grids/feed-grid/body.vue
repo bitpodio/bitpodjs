f254f8440788f75d7c1ebd747e07b6034186d79f
rishikesh <rishikesh@bitpod.io>
2021-05-11 20:24:32 +0530

    body template added for feed

---
+++ config/templates/grids/feed-grid/body.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="greybg">
+    <v-card
+      v-for="item in items"
+      :key="item.id"
+      max-width="600"
+      class="mb-4 mr-4"
+    >
+      <v-list subheader two-line>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-avatar size="48" color="primary">
+              <span class="white--text Twitter">{{ item.createdBy }}</span>
+            </v-avatar>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.createdBy }}</v-list-item-title>
+
+            <v-list-item-subtitle> {{ item.createdDate }}</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon color="grey lighten-1">mdi-information</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+      <div class="mx-4 mb-2">
+        <div class="mt-n2 mb-2">{{ item.Title }}</div>
+        <div class="mb-2">{{ item.Body }}</div>
+      </div>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    items: { type: Array, default: () => [] },
+    offset: { type: Boolean, default: false },
+    refresh: {
+      type: Function,
+      default: () => false,
+      required: false,
+    },
+  },
+  methods: {
+    getRandomColor(createdBy) {
+      return window.GeoPattern.generate(createdBy).color
+    },
+  },
+}
+</script>
