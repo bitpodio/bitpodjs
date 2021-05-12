a36e9f8499071428396342c1c28b24911ba0020f
Amartya Roy <amartyakumar@bitpod.io>
2021-05-12 11:21:37 +0530

    Fixed issue of merge conflict changes was reverted

--- config/apps/member/content/member.js
+++ config/apps/member/content/member.js
@@ -9,8 +9,8 @@ export default {
   views: {
     Members: {
       ui: {
-        hideDefaultHeader: false,
-        hideDefaultFooter: false,
+        hideDefaultHeader: true,
+        hideDefaultFooter: true,
         showExpand: false,
         singleExpand: false,
         showSelect: true,
@@ -266,6 +266,267 @@ export default {
         defaultSort: 'createdDate DESC',
       },
       title: 'Drawer.Members',
+      defaultSort: 'createdDate DESC',
+    },
+    Member: {
+      ui: {
+        hideDefaultHeader: false,
+        hideDefaultFooter: false,
+        showExpand: false,
+        singleExpand: false,
+        showSelect: true,
+        hideFilter: false,
+        hideSearch: false,
+      },
+      itemTitle: 'Common.Members',
+      default: false,
+      fields: {
+        FirstName: {
+          form: {
+            caption: 'Common.FirstName',
+            displayOrder: 1,
+          },
+          displayOrder: 2,
+          caption: 'Common.FirstNameCaption',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '150px',
+          type: 'string',
+          cssClasses: 'col-12 col-md-6',
+          hidden: false,
+          inlineEdit: true,
+          newForm: true,
+          editForm: true,
+          rules: [
+            function (v) {
+              return !!v || this.$t('Messages.Error.FirstNameRequired')
+            },
+          ],
+        },
+        LastName: {
+          form: {
+            caption: 'Common.LastName',
+            displayOrder: 2,
+          },
+          displayOrder: 3,
+          caption: 'Common.LastNameCaption',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '150px',
+          type: 'string',
+          cssClasses: 'col-12 col-md-6',
+          hidden: false,
+          inlineEdit: true,
+          newForm: true,
+          editForm: true,
+          rules: [
+            function (v) {
+              return !!v || this.$t('Messages.Error.LastNameRequired')
+            },
+          ],
+        },
+        Email: {
+          form: {
+            caption: 'Common.Email',
+            displayOrder: 3,
+          },
+          displayOrder: 4,
+          caption: 'Common.EmailCaption',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '130px',
+          type: 'string',
+          cssClasses: 'col-12 col-md-6',
+          hidden: false,
+          inlineEdit: true,
+          newForm: true,
+          editForm: true,
+          rules: [
+            function (v) {
+              return !!v || this.$t('Messages.Error.EmailRequired')
+            },
+            function (value, data) {
+              return /.+@.+\..+/.test(value) || 'E-mail must be valid'
+            },
+          ],
+        },
+        BusinessNumber: {
+          form: {
+            caption: 'Common.PhoneRequired',
+            displayOrder: 4,
+          },
+          displayOrder: 5,
+          caption: 'Common.BusinessNumber',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '165px',
+          type: 'string',
+          cssClasses: 'col-12 col-md-6',
+          hidden: false,
+          inlineEdit: true,
+          newForm: true,
+          editForm: true,
+          rules: [
+            function (v) {
+              return !!v || this.$t('Messages.Error.PhoneRequired')
+            },
+            function (value, data) {
+              return /^[0-9]\d*$|^$/.test(value) || 'Number must be valid'
+            },
+          ],
+        },
+        createdDate: {
+          displayOrder: 6,
+          caption: 'Common.CreatedDate',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '150px',
+          type: 'date',
+          newForm: false,
+          editForm: false,
+          customExport: (startDate) => {
+            if (startDate) {
+              const tempDate = new Date(startDate)
+              return (
+                tempDate.toLocaleDateString() +
+                ' ' +
+                tempDate.toLocaleTimeString()
+              )
+            }
+            return ''
+          },
+        },
+        createdBy: {
+          displayOrder: 7,
+          caption: 'Common.CreatedBy',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '130px',
+          type: 'string',
+          newForm: false,
+          editForm: false,
+        },
+        Tags: {
+          form: {
+            caption: 'Common.Tags',
+            displayOrder: 5,
+          },
+          caption: 'Common.Tags',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '130px',
+          type: 'tags',
+          hidden: true,
+          newForm: true,
+          editForm: true,
+          cssClasses: 'col-12 col-md-12',
+        },
+        '_BillingAddress.AddressLine': {
+          form: {
+            caption: 'Common.Address',
+            displayOrder: 6,
+          },
+          caption: 'Common.Address',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '130px',
+          type: 'string',
+          hidden: true,
+          newForm: true,
+          editForm: true,
+          cssClasses: 'col-12 col-md-12',
+        },
+        '_BillingAddress.City': {
+          form: {
+            caption: 'Common.City',
+            displayOrder: 7,
+          },
+          caption: 'Common.City',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '130px',
+          type: 'string',
+          hidden: true,
+          newForm: true,
+          editForm: true,
+          cssClasses: 'col-12 col-md-6',
+        },
+        '_BillingAddress.State': {
+          form: {
+            caption: 'Common.State',
+            displayOrder: 8,
+          },
+          caption: 'Common.State',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '130px',
+          type: 'string',
+          hidden: true,
+          newForm: true,
+          editForm: true,
+          cssClasses: 'col-12 col-md-6',
+        },
+        '_BillingAddress.PostalCode': {
+          form: {
+            caption: 'Common.Zip',
+            displayOrder: 9,
+          },
+          caption: 'Common.Zip',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '130px',
+          type: 'string',
+          hidden: true,
+          newForm: true,
+          editForm: true,
+          cssClasses: 'col-12 col-md-6',
+        },
+        '_BillingAddress.Country': {
+          form: {
+            caption: 'Common.Country',
+            displayOrder: 10,
+          },
+          caption: 'Common.Country',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '130px',
+          type: 'string',
+          hidden: true,
+          newForm: true,
+          editForm: true,
+          cssClasses: 'col-12 col-md-6',
+        },
+        Description: {
+          form: {
+            caption: 'Common.Description',
+            displayOrder: 11,
+          },
+          caption: 'Common.Description',
+          searchEnable: true,
+          sortEnable: true,
+          columnWidth: '130px',
+          type: 'string',
+          hidden: true,
+          newForm: true,
+          editForm: true,
+          cssClasses: 'col-12 col-md-12',
+        },
+      },
+      template: {
+        name: 'memberList-grid',
+        context: {
+          basePath: '/member',
+        },
+      },
+      dataSource: {
+        singularEntity: 'Common.Members',
+        pluralEntity: 'Drawer.Members',
+        query: memberList,
+        type: 'graphql',
+        model: 'Customer',
+        defaultSort: 'createdDate DESC',
+      },
+      title: 'Common.MemberList',
       defaultSort: 'createdDate DESC',
     },
     memberSubscription: {
@@ -1021,6 +1282,7 @@ export default {
         defaultSort: 'createdDate DESC',
       },
       itemTitle: 'Common.Contact',
+      hidden: true,
       title: 'Common.Contacts',
       defaultSort: 'createdDate DESC',
     },
@@ -1486,6 +1748,7 @@ export default {
         },
         defaultSort: 'createdDate DESC',
       },
+      hidden: true,
       title: 'Drawer.Members',
       defaultSort: 'createdDate DESC',
     },
@@ -1637,6 +1900,7 @@ export default {
         },
         defaultSort: 'createdDate DESC',
       },
+      hidden: true,
       itemTitle: 'Common.Contact',
       title: 'Common.Contacts',
       defaultSort: 'createdDate DESC',
