e838b44c6591c287ae205d5721ea87a48550cb3e
Lokesh Popat <lpopat@paragyte.com>
2021-05-13 12:50:59 +0530

    removed nuxt.config changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -101,7 +101,7 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    // '@nuxtjs/eslint-module',
+    '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
     '@nuxtjs/vuetify',
@@ -253,7 +253,7 @@ export default {
   axios: {
     googleMapUrl: `https://www.google.com/maps`,
     apiEndpoint: '/svc/api/',
-    // baseURL: `https://${process.env.PUBLIC_DOMAIN}${basePath}`,
+    baseURL: `https://${process.env.PUBLIC_DOMAIN}${basePath}`,
   },
 
   publicRuntimeConfig: {
@@ -337,7 +337,7 @@ export default {
         httpEndpoint: `https://${
           process.env.PUBLIC_DOMAIN || 'event.test.bitpod.io'
         }/svc/graphql`,
-        // browserHttpEndpoint: '/svc/graphql',
+        browserHttpEndpoint: '/svc/graphql',
       },
     },
     defaultOptions: {
