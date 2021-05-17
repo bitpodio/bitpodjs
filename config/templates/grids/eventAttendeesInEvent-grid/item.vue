8fb22d8c414ee92fecb5ed3cb106861a9e8b57a1
rishikesh <rishikesh@bitpod.io>
2021-05-17 13:11:29 +0530

    added new files for event attendee as event

---
+++ config/templates/grids/eventAttendeesInEvent-grid/item.vue
@@ -0,0 +1,87 @@
+<template>
+  <tr>
+    <td>
+      <div>
+        <v-list subheader two-line class="px-0">
+          <v-list-item class="px-0">
+            <v-list-item-avatar size="48">
+              <v-avatar
+                size="48"
+                :style="{
+                  'background-color': getRandomColor(item.FullName),
+                }"
+              >
+                <span class="white--text Twitter">{{ item.FullName }}</span>
+              </v-avatar>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.FullName }}</v-list-item-title>
+
+              <v-list-item-subtitle>
+                {{ item.Email }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <div class="min-w-checkin">
+                <ColumnCheckin
+                  :item="item"
+                  :context="context"
+                  :refresh="refresh"
+                  :is-custom-mobile="true"
+                />
+              </div>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </div>
+    </td>
+  </tr>
+</template>
+<script>
+import ColumnCheckin from './column-checkin.vue'
+export default {
+  components: {
+    ColumnCheckin,
+  },
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+      required: false,
+    },
+    context: {
+      type: Object,
+      default: () => {},
+      required: false,
+    },
+    refresh: {
+      type: Function,
+      default: () => false,
+      required: false,
+    },
+    isSelected: {
+      type: Boolean,
+      default: false,
+    },
+    select: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  methods: {
+    getRandomColor(Name) {
+      return window.GeoPattern.generate(Name).color
+    },
+  },
+}
+</script>
+<style scoped>
+.min-w-checkin {
+  min-width: 88px;
+}
+.fs-larger {
+  font-size: 1.25rem;
+}
+</style>
