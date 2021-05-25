4c1f53732f2f503717b2110e218f4333e279ee84
Yash Dave <74710904+yash-bitpod@users.noreply.github.com>
2021-05-25 19:31:39 +0530

    Update form.vue

--- config/templates/grids/discount-grid/actions/form.vue
+++ config/templates/grids/discount-grid/actions/form.vue
@@ -331,10 +331,10 @@ export default {
           this.uniqueCodeMessage = ''
         }
       } catch (error) {
-        // console.error(
-        //   `Error in config/templates/grids/discount-grid/actions/form.vue while checking unique code context: OfferCode GQL, filter:${where}`,
-        //   error
-        // )
+         console.error(
+           `Error in config/templates/grids/discount-grid/actions/form.vue while checking unique code context: OfferCode GQL, filter:${where}`,
+           error
+         )
       }
     },
     async onSave() {
