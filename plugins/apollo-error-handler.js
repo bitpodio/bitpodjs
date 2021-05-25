30b98a8128050b9b46960b5959c86343a064cf13
Amartya Roy <amartyakumar@bitpod.io>
2021-05-25 19:03:48 +0530

    removed loggers

--- plugins/apollo-error-handler.js
+++ plugins/apollo-error-handler.js
@@ -6,30 +6,21 @@ export default async (
     let token = ''
     if (nuxtContext.$auth.$storage.getCookies()['auth._token.bitpod']) {
       token = nuxtContext.$auth.$storage.getCookies()['auth._token.bitpod']
-      console.debug('token1--->', token)
     } else {
       token = nuxtContext.$auth.$storage.getCookies()['auth._token.google']
-      console.debug('token2--->', token)
     }
     const strategy = nuxtContext.$auth.$storage.getCookies()['auth.strategy']
-    console.debug('strategy--->', strategy)
     if (strategy === 'bitpod' || strategy === 'google') {
-      console.debug(
-        'apollo---->',
-        nuxtContext.$auth.$storage.getCookies()['apollo-token']
-      )
       if (
         token.split(' ')[1] !==
         nuxtContext.$auth.$storage.getCookies()['apollo-token']
       ) {
         let newToken = nuxtContext.$auth.strategy.token.get()
-        console.debug('newToken--->', newToken)
         if (newToken) {
           newToken = newToken.split(' ')[1]
           await nuxtContext.$apolloHelpers.onLogin(newToken, undefined, {
             expires: 7,
           })
-          console.debug('redirecting --->', newToken)
         }
       }
       nuxtContext.redirect(301, `/apps/event/eventboard`)
