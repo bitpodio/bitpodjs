eb6fe42b31439beed077cbc22d136ed4c1c7dff7
rishikesh <rishikesh@bitpod.io>
2021-06-08 14:42:46 +0530

    esign body template changes

--- config/templates/grids/eSign-grid/body.vue
+++ config/templates/grids/eSign-grid/body.vue
@@ -1,198 +1,202 @@
 <template>
   <div>
-    <v-flex class="greybg">
+    <div class="greybg">
       <div class="esign-action">
         <v-btn text small @click="handleNewTemplate">
           <v-icon left class="fs-16">mdi-plus</v-icon
           ><i18n path="Common.SendDocforeSign" />
         </v-btn>
       </div>
-      <v-col cols="12" class="overflowHidden px-0">
-        <div
-          style="width: 100%;"
-          :class="{
-            'd-flex flex-nowrap': true,
-            'summary-block': $vuetify.breakpoint.smAndDown,
-          }"
-        >
+      <div>
+        <v-col cols="12" class="overflowHidden px-0">
           <div
-            v-for="(data, index) in requestSummaryData"
-            :key="index"
-            class="white d-inline-block elevation-2 rounded flex-grow-0 flex-shrink-0"
-            :class="{ 'mr-5': index < 4 }"
-            style="width: 200px; height: 60px;"
+            style="width: 100%;"
+            :class="{
+              'd-flex flex-nowrap': true,
+              'summary-block': $vuetify.breakpoint.smAndDown,
+            }"
           >
-            <nuxt-link
-              :to="eSignViews(data.view)"
-              class="text-decoration-none black--text"
+            <div
+              v-for="(data, index) in requestSummaryData"
+              :key="index"
+              class="white d-inline-block elevation-2 rounded flex-grow-0 flex-shrink-0"
+              :class="{ 'mr-5': index < 4 }"
+              style="width: 200px; height: 60px;"
             >
-              <v-skeleton-loader
-                :loading="false"
-                :tile="true"
-                type="avatar"
-                height="60"
-                width="60"
+              <nuxt-link
+                :to="eSignViews(data.view)"
+                class="text-decoration-none black--text"
               >
-                <div class="d-flex">
-                  <div
-                    style="width: 60px; height: 60px;"
-                    :class="data.class"
-                    class="rounded-l"
-                  >
-                    <v-icon
-                      style="font-size: 28px;"
-                      class="pl-4 pt-4 white--text"
-                      >{{ data.icon }}</v-icon
+                <v-skeleton-loader
+                  :loading="false"
+                  :tile="true"
+                  type="avatar"
+                  height="60"
+                  width="60"
+                >
+                  <div class="d-flex">
+                    <div
+                      style="width: 60px; height: 60px;"
+                      :class="data.class"
+                      class="rounded-l"
                     >
+                      <v-icon
+                        style="font-size: 28px;"
+                        class="pl-4 pt-4 white--text"
+                        >{{ data.icon }}</v-icon
+                      >
+                    </div>
+                    <div class="pl-2 pt-1">
+                      <h3 class="font-weight-regular text-truncate summaryTile">
+                        {{ data.value }}
+                      </h3>
+                      <h5 class="font-weight-regular text-truncate summaryTile">
+                        {{ data.caption }}
+                      </h5>
+                    </div>
                   </div>
-                  <div class="pl-2 pt-1">
-                    <h3 class="font-weight-regular text-truncate summaryTile">
-                      {{ data.value }}
-                    </h3>
-                    <h5 class="font-weight-regular text-truncate summaryTile">
-                      {{ data.caption }}
-                    </h5>
-                  </div>
-                </div>
-              </v-skeleton-loader>
-            </nuxt-link>
+                </v-skeleton-loader>
+              </nuxt-link>
+            </div>
           </div>
-        </div>
-      </v-col>
-      <v-col class="d-flex flex-wrap greybg pa-0 pt-10">
-        <v-col
-          v-for="item in items"
-          :key="item.id"
-          class="pa-4 pl-0 pt-0 eventtiles ma-sm-4 ml-sm-0 mt-sm-0 mx-auto my-2"
-        >
-          <nuxt-link :to="eSignRoutes(item.id)" class="text-decoration-none">
-            <v-card class="elevation-0 pa-0">
-              <div class="positionRelative">
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                      :class="{
-                        'ma-2 positionAbsolute request-status text-darken-4': true,
-                        'success--text': checkStatus(
-                          item.Status,
-                          'Completed',
-                          item.ExpirationDate
-                        ),
-                        'info--text': checkStatus(
-                          item.Status,
-                          'Inprogress',
-                          item.ExpirationDate
-                        ),
-                        'warning--text': checkStatus(
-                          item.Status,
-                          'Pending',
-                          item.ExpirationDate
-                        ),
-                        'error--text': checkStatus(
-                          item.Status,
-                          'Declined',
-                          item.ExpirationDate
-                        ),
-                        'grey--text': checkIfExpired(
-                          item.Status,
-                          item.ExpirationDate
-                        ),
-                      }"
-                      depressed
-                      small
-                      color="white"
-                      v-bind="attrs"
-                      v-on="on"
-                    >
-                      <i18n
-                        v-if="
-                          checkStatus(
+        </v-col>
+      </div>
+      <div>
+        <div class="d-flex flex-wrap flex-column flex-sm-row greybg pa-0 pt-10">
+          <v-col
+            v-for="item in items"
+            :key="item.id"
+            class="pa-4 pl-0 pt-0 eventtiles ma-sm-4 ml-sm-0 mt-sm-0 mx-auto my-2"
+          >
+            <nuxt-link :to="eSignRoutes(item.id)" class="text-decoration-none">
+              <v-card class="elevation-0 pa-0">
+                <div class="positionRelative">
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        :class="{
+                          'ma-2 positionAbsolute request-status text-darken-4': true,
+                          'success--text': checkStatus(
                             item.Status,
                             'Completed',
                             item.ExpirationDate
-                          )
-                        "
-                        path="Common.Completed"
-                      />
-                      <i18n
-                        v-else-if="
-                          checkStatus(
+                          ),
+                          'info--text': checkStatus(
                             item.Status,
                             'Inprogress',
                             item.ExpirationDate
-                          )
-                        "
-                        path="Common.OutForSignature"
-                      />
-                      <i18n
-                        v-else-if="
-                          checkStatus(
+                          ),
+                          'warning--text': checkStatus(
                             item.Status,
                             'Pending',
                             item.ExpirationDate
-                          )
-                        "
-                        path="Common.Pending"
-                      />
-                      <i18n
-                        v-else-if="
-                          checkStatus(
+                          ),
+                          'error--text': checkStatus(
                             item.Status,
                             'Declined',
                             item.ExpirationDate
-                          )
-                        "
-                        path="Common.Declined"
-                      />
-                      <i18n v-else path="Common.Expired" />
-                    </v-btn>
-                  </template>
-                  <div v-if="item.Recipient && item.Recipient.edges">
-                    <div
-                      v-for="(recipient, index) in item.Recipient.edges"
-                      :key="index"
-                      class="h1 recipient-status-info"
-                    >
-                      {{
-                        recipient.node.Status
-                          ? `${recipient.node.FullName} [${recipient.node.type}]: ${recipient.node.Status}`
-                          : `${recipient.node.FullName} [${recipient.node.type}]: Pending`
-                      }}
+                          ),
+                          'grey--text': checkIfExpired(
+                            item.Status,
+                            item.ExpirationDate
+                          ),
+                        }"
+                        depressed
+                        small
+                        color="white"
+                        v-bind="attrs"
+                        v-on="on"
+                      >
+                        <i18n
+                          v-if="
+                            checkStatus(
+                              item.Status,
+                              'Completed',
+                              item.ExpirationDate
+                            )
+                          "
+                          path="Common.Completed"
+                        />
+                        <i18n
+                          v-else-if="
+                            checkStatus(
+                              item.Status,
+                              'Inprogress',
+                              item.ExpirationDate
+                            )
+                          "
+                          path="Common.OutForSignature"
+                        />
+                        <i18n
+                          v-else-if="
+                            checkStatus(
+                              item.Status,
+                              'Pending',
+                              item.ExpirationDate
+                            )
+                          "
+                          path="Common.Pending"
+                        />
+                        <i18n
+                          v-else-if="
+                            checkStatus(
+                              item.Status,
+                              'Declined',
+                              item.ExpirationDate
+                            )
+                          "
+                          path="Common.Declined"
+                        />
+                        <i18n v-else path="Common.Expired" />
+                      </v-btn>
+                    </template>
+                    <div v-if="item.Recipient && item.Recipient.edges">
+                      <div
+                        v-for="(recipient, index) in item.Recipient.edges"
+                        :key="index"
+                        class="h1 recipient-status-info"
+                      >
+                        {{
+                          recipient.node.Status
+                            ? `${recipient.node.FullName} [${recipient.node.type}]: ${recipient.node.Status}`
+                            : `${recipient.node.FullName} [${recipient.node.type}]: Pending`
+                        }}
+                      </div>
                     </div>
+                  </v-tooltip>
+                  <div class="overflow-h rounded-t">
+                    <v-flex
+                      class="tile-img tile-pattern rounded-0"
+                      :style="{
+                        'background-image': getRandomImage(item.id),
+                      }"
+                    ></v-flex>
                   </div>
-                </v-tooltip>
-                <div class="overflow-h rounded-t">
-                  <v-flex
-                    class="tile-img tile-pattern rounded-0"
-                    :style="{
-                      'background-image': getRandomImage(item.id),
-                    }"
-                  ></v-flex>
                 </div>
-              </div>
-              <v-flex class="tile-info pa-4 pb-0">
-                <div class="text--secondary pa-2 pb-0 body-2 pl-0 pt-0">
-                  {{ $d(new Date(item.createdDate), 'long', $i18n.locale) }}
-                </div>
-                <v-card-title
-                  class="text-h5 grey--text text--darken-4 text-truncate d-block text-capitalize pa-2 pt-0 pb-1 pl-0"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  {{ item.Subject }}
-                </v-card-title>
-              </v-flex>
-              <v-card-actions class="pt-0 pl-4 tiles-action">
-                <div class="text-truncate d-block">
-                  {{ item.createdBy }}
-                </div>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </nuxt-link>
-        </v-col>
-      </v-col>
-    </v-flex>
+                <v-flex class="tile-info pa-4 pb-0">
+                  <div class="text--secondary pa-2 pb-0 body-2 pl-0 pt-0">
+                    {{ $d(new Date(item.createdDate), 'long', $i18n.locale) }}
+                  </div>
+                  <v-card-title
+                    class="text-h5 grey--text text--darken-4 text-truncate d-block text-capitalize pa-2 pt-0 pb-1 pl-0"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    {{ item.Subject }}
+                  </v-card-title>
+                </v-flex>
+                <v-card-actions class="pt-0 pl-4 tiles-action">
+                  <div class="text-truncate d-block">
+                    {{ item.createdBy }}
+                  </div>
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </nuxt-link>
+          </v-col>
+        </div>
+      </div>
+    </div>
     <div v-if="dialog">
       <ESignForm :new-template-dialog.sync="dialog" :refresh="refresh" />
     </div>
@@ -387,5 +391,10 @@ export default {
   font-size: 14px !important;
   padding: 0.5em !important;
   background-color: white !important;
+}
+@media (max-width: 600px) {
+  .summary-block {
+    max-width: 94vw;
+  }
 }
 </style>
