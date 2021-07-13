4e9904d7cd05b6e3819113c8283def7fcb4c7c22
Amartya Roy <amartyakumar@bitpod.io>
2021-07-13 20:22:46 +0530

    added function to add hours and minutes based on timezone offset

--- config/templates/grids/userprofile-grid/column-timestamp.vue
+++ config/templates/grids/userprofile-grid/column-timestamp.vue
@@ -2,12 +2,11 @@
   <div v-if="date !== undefined && date && Timezone !== ''">
     {{ getTimeStampDate(date, userTimezone) }}
   </div>
-  <div v-else>
-    {{ $d(new Date(date), 'long', $i18n.locale) }}
-  </div>
 </template>
 
 <script>
+import add from 'date-fns/add'
+import sub from 'date-fns/sub'
 export default {
   props: {
     item: {
@@ -24,6 +23,10 @@ export default {
     return {
       date: this.item.timeStamp,
       userTimezone: this.$store.state.userProfileTimezone || '',
+      dateObj: {
+        hours: '',
+        minutes: '',
+      },
     }
   },
   methods: {
@@ -36,17 +39,30 @@ export default {
       return newDate
     },
     getTimeStampDate(timestamp, Timezone) {
-      const timezoneOffset = parseInt(
-        this.userTimezone.split(')')[0].split('UTC')[1]
-      )
-      const convertedDate =
-        new Date(timestamp).getTime() + 3600000 * timezoneOffset
-      const newDate = this.$d(
-        new Date(convertedDate),
-        'long',
-        this.$i18n.locale
-      )
-      return newDate
+      if (Timezone !== '') {
+        let convertedDate
+        const timezoneOffset = Timezone.match(/([A-Z]+[\\+-][0-9]+.*)/)[1]
+          .split(' ')[0]
+          .split('UTC')[1]
+          .split(')')[0]
+          .split(':')
+        this.dateObj.hours = parseInt(timezoneOffset[0])
+        this.dateObj.minutes = parseInt(timezoneOffset[1])
+        if (timezoneOffset[0].includes('+')) {
+          convertedDate = add(new Date(timestamp), this.dateObj)
+        } else {
+          convertedDate = sub(new Date(timestamp), this.dateObj)
+        }
+        const newDate = this.$d(
+          new Date(convertedDate),
+          'long',
+          this.$i18n.locale
+        )
+        return newDate
+      } else {
+        const newDate = this.$d(new Date(timestamp), 'long', this.$i18n.locale)
+        return newDate
+      }
     },
   },
 }
